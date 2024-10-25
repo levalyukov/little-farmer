8c51b05ffc8e88e82f06c89cb36a045d2bfb0d6f
Lev Alyukov <lev.alyukov@gmail.com>
2024-10-25 14:37:12 +0500

    update: shadow.gd

--- assets/scripts/world/shadow.gd
+++ assets/scripts/world/shadow.gd
@@ -13,8 +13,14 @@ const max_height_map:int = 23
 const min_height_map:int = -16
 
 const max_distance:int = 525
-const max_clouds:int = 32
-var all_clouds:int = 0
+
+var max_clouds:int = 100
+var clouds_value:int = 0
+
+func change_clouds_value(value:int = 5) -> void:
+	if value >= 0:
+		clouds_value = value
+
 
 func create_shadow(shadow_name:String, shadow_texture:CompressedTexture2D, shadow_position:Vector2i) -> void:
 	if shadow_name == "":
@@ -26,16 +32,16 @@ func create_shadow(shadow_name:String, shadow_texture:CompressedTexture2D, shado
 	canvas.add_child(shadow)
 
 func create_cloud(shadow_texture:CompressedTexture2D) -> void:
-	if all_clouds < max_clouds:
+	if clouds_value < max_clouds:
 		var node:PackedScene = load("res://assets/nodes/world/cloud.tscn")
 		var cloud = node.instantiate()
 		var cloud_position_x = randi_range(min_widht_map, max_widht_map)
 		var cloud_position_y = randi_range(min_height_map, max_height_map)
 		var target_position = Vector2i(cloud_position_x, cloud_position_y)
 		
-		cloud.name = "cloud_"+ str(all_clouds)
+		cloud.name = "cloud_"+ str(clouds_value)
 		cloud.texture = shadow_texture
 		cloud.position = tilemap.map_to_local(target_position)
 		canvas.add_child(cloud)
 		cloud.change_animation(true)
-		all_clouds += 1
+		clouds_value += 1
