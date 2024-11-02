76c078f271bc291e234b86689c547addfc081a2a
Lev Alyukov <lev.alyukov@gmail.com>
2024-11-02 11:42:49 +0500

    add: traders.gd

---
+++ assets/scripts/ui/interactive/trading/traders.gd
@@ -0,0 +1,25 @@
+extends Node
+
+class_name Traders
+var content:Dictionary = {
+    1: {
+        "inventory" = {
+            13:{
+                "max": 25,
+                "amount": 25,
+                },
+            14:{
+                "max": 25,
+                "amount": 25,
+                },
+            15:{
+                "max": 25,
+                "amount": 25,
+                },
+            16:{
+                "max": 25,
+                "amount": 25,
+                },
+        }
+    }
+}
