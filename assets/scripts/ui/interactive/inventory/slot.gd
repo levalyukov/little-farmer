c8cd0ee04b977d880ff4a517f8edf2c0e7152089
Lev Alyukov <lev.alyukov@gmail.com>
2024-11-02 11:41:20 +0500

    update: slot.gd

--- assets/scripts/ui/interactive/inventory/slot.gd
+++ assets/scripts/ui/interactive/inventory/slot.gd
@@ -6,14 +6,19 @@ extends Control
 @onready var inventory:Control = get_node("/root/"+main+"/UI/Interactive/Inventory")
 @onready var mailbox:Control = get_node("/root/"+main+"/UI/Interactive/Mailbox")
 @onready var signmenu:Control = get_node("/root/"+main+"/UI/Interactive/BuildingsMenu/SignMenu")
+@onready var trade_menu:Control = get_node("/root/"+main+"/UI/Interactive/TradeMenu")
 @onready var buildings:Node = get_node("/root/"+main+"/ConstructionManager")
 @onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
 @onready var icon:TextureRect = $Button/Icon
 @onready var amount_label:Label = $Button/Amount
 
+@onready var storage:Node2D = get_node("/root/"+main+"/ConstructionManager/Storage")
+
 var id
 var amount:int
 var item:Object = Items.new()
+enum tr_initator {NONE, PLAYER, TRADER}
+var tr_arg:int = 0
 
 func set_data(index, item_amount) -> void:
 	self.id = index
@@ -71,11 +76,27 @@ func _on_button_mouse_entered():
 			else:
 				data.debug("Invalid item ID: " + str(id), "warning")
 
+	if has_node("/root/"+main+"/UI/Interactive/TradeMenu"):
+		if trade_menu.visible:
+			if blur.state:
+				if id:
+					if item.content.has(int(id)):
+						if item.content[int(id)].has("caption"):
+							tip.tooltip(
+								item.content[int(id)]["caption"]
+								)
+						else:
+							data.debug("The 'caption' key is missing.", "error")
+					else:
+						data.debug("Invalid item ID: " + str(id), "warning")
+
 func _on_button_mouse_exited():
 	if has_node("/root/"+main+"/UI/Interactive/Mailbox")\
-	|| has_node("/root/"+main+"/UI/Interactive/BuildingsMenu/SignMenu"):
+	|| has_node("/root/"+main+"/UI/Interactive/BuildingsMenu/SignMenu")\
+	|| has_node("/root/"+main+"/UI/Interactive/TradeMenu"):
 		if mailbox.menu\
-		|| signmenu.menu:
+		|| signmenu.menu\
+		|| trade_menu.menu:
 			tip.tooltip("")
 
 func _on_button_pressed():
@@ -90,3 +111,35 @@ func _on_button_pressed():
 					if i.name == signmenu.sign_name:
 						i.set_sign_sprite(int(id))
 						signmenu._close()
+
+	if has_node("/root/"+main+"/UI/Interactive/TradeMenu"):
+		match tr_arg:
+			tr_initator.PLAYER:
+				if trade_menu:
+					if trade_menu.initiator == trade_menu.initiators.PLAYER || trade_menu.initiator == trade_menu.initiators.NONE:
+						trade_menu.add_item_trade_window(id, tr_initator.PLAYER)
+						trade_menu.updates_arrays()
+						trade_menu.get_target_price()
+						trade_menu.update_button_trade_window()
+			tr_initator.TRADER:
+				if trade_menu:
+					if trade_menu.initiator == trade_menu.initiators.TRADER || trade_menu.initiator == trade_menu.initiators.NONE:
+						trade_menu.add_item_trade_window(id, tr_initator.TRADER)
+						trade_menu.updates_arrays()
+						trade_menu.get_target_price()
+						trade_menu.update_button_trade_window()
+			tr_initator.NONE:
+				if trade_menu:
+					if trade_menu.initiator == trade_menu.initiators.PLAYER || trade_menu.initiator == trade_menu.initiators.TRADER:
+						trade_menu.remove_item_trade_window(id)
+						if trade_menu.initiator == trade_menu.initiators.TRADER:
+							trade_menu.updates_arrays()
+						trade_menu.update_button_trade_window()
+						trade_menu.get_target_price()
+			_:
+				pass
+		print("Initiator: ", trade_menu.initiator)
+		print("Aviabled slots: ", str(storage.object[storage.level]["slots"] - inventory.get_all_items()))
+		print("Trade slots: ", trade_menu.get_all_items_in_trade_window())
+		print("New items in inventory: ", trade_menu.new_items_in_inventory)
+		print("Simillar items: ", trade_menu.simillar_items)
