11cb7d70840d09ed013f893c166e39c12f3cbabb
Lev Alyukov <lev.alyukov@gmail.com>
2024-11-13 09:19:10 +0500

    fix: path in button_construct.gd

--- assets/scripts/ui/hud/tools/button_construct.gd
+++ assets/scripts/ui/hud/tools/button_construct.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var main:String = str(get_tree().root.get_child(1).name)
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
-@onready var craft:Control = get_node("/root/"+main+"/UI/Interactive/Crafting")
+@onready var craft:Control = get_node("/root/"+main+"/UI/Interactive/ConstructMenu")
 @onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
 @onready var sprite:CompressedTexture2D = load("res://assets/resources/ui/interactive/hud/tools/hammer.png")
 @onready var icon:TextureRect = $Main/Margin/Icon
