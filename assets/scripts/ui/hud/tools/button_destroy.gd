714014df383f4046ce0ec71469d887520d7aff80
Lev Alyukov <lev.alyukov@gmail.com>
2024-11-26 12:43:53 +0500

    Update button_destroy.gd

--- assets/scripts/ui/hud/tools/button_destroy.gd
+++ assets/scripts/ui/hud/tools/button_destroy.gd
@@ -8,6 +8,8 @@ extends Control
 @onready var sprite:CompressedTexture2D = load("res://assets/resources/ui/interactive/hud/tools/eraser.png")
 @onready var icon:TextureRect = $Main/Margin/Icon
 
+@onready var destroy_menu:Control = get_node("/root/"+main+"/UI/HUD/GameHud/Main/DestroyMenuMargin/DestroyMenu")
+
 func _ready() -> void:
 	icon.texture = sprite
 
@@ -16,8 +18,8 @@ func _on_button_pressed() -> void:
 		if has_node("/root/"+main+"/ConstructionManager")\
 		&& has_node("/root/"+main+"/ConstructionManager/Grid"):
 			if !blur.state:
-				grid.grid_dimensions = tools.features["destroy"][tools.destroy]["grid_dimensions"]
-				grid.mode = grid.modes.DESTROY
-				grid.visible = true
-				grid.generate_grid()
+				if !destroy_menu.opened:
+					destroy_menu.open()
+				else:
+					destroy_menu.close()
 
