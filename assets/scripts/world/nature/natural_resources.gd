e6dc6b5a2ba882944c11cc08cc0d8eb881eeb855
Lev Alyukov <lev.alyukov@gmail.com>
2024-11-28 20:21:53 +0500

    add: new class "natural_resources.gd"

---
+++ assets/scripts/world/nature/natural_resources.gd
@@ -0,0 +1,13 @@
+extends Node
+
+class_name NaturalResources
+var content:Dictionary = {
+    "tree": {
+        "item_id": 1,
+        "item_count": [1,10]
+    },
+    "stone": {
+        "item_id": 3,
+        "item_count": [1,3]
+    },
+}
