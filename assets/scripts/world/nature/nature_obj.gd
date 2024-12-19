91547add5251f98c6dae497f6924abfd52851d73
levalyukov <lev.alyukov@gmail.com>
2024-12-19 12:16:47 +0500

    update: nature_obj.gd

--- assets/scripts/world/nature/nature_obj.gd
+++ assets/scripts/world/nature/nature_obj.gd
@@ -2,12 +2,17 @@ extends Node2D
 
 @onready var main:String = str(get_tree().root.get_child(1).name)
 @onready var data:Node2D = get_node("/root/"+main)
+@onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var sprite:Sprite2D = $Sprite2D
 
+var type:String = ""
 var health:int = 1
+var index:int = 0
 
-func set_texture(target_sprite:CompressedTexture2D) -> void:
+func set_texture(target_sprite:CompressedTexture2D, texture_index:int = 0) -> void:
 	sprite.texture = target_sprite
+	index = texture_index
+	type = data.remove_suffix(self.name)
 	set_health()
 
 func set_health():
@@ -19,4 +24,12 @@ func set_health():
 		"stone":
 			health = 2
 		_:
-			pass
+			health = 1
+
+func get_data():
+	return {
+		"type": type,
+		"sprite_index": index,
+		"position": tilemap.local_to_map(global_position),
+		"health": health,
+	}
