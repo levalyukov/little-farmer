37b31f98657f7d1668ecd5b01188efb442dcf95e
levalyukov <lev.alyukov@gmail.com>
2024-12-28 21:54:00 +0500

    fix: gameloader.gd

--- assets/data/gameloader.gd
+++ assets/data/gameloader.gd
@@ -1,10 +1,4 @@
 extends Node
 
 var start:bool = false
-var mode:bool
-
-func loading(transfer_data:bool) -> void:
-	mode = transfer_data
-
-func gamestart(transfer_data:bool) -> void:
-	start = transfer_data
+var mode:bool = false
