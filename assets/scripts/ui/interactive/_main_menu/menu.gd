0a1988b99c8f338511aef0ebcfc7f7b5dad1fc98
levalyukov <lev.alyukov@gmail.com>
2024-12-28 20:41:55 +0500

    update: menu.gd

--- assets/scripts/ui/interactive/_main_menu/menu.gd
+++ assets/scripts/ui/interactive/_main_menu/menu.gd
@@ -1,92 +1,40 @@
-extends Node2D
+extends MarginContainer
 
 @onready var main = str(get_tree().root.get_child(1).name)
-@onready var blur:Control = get_node("/root/"+main+"/Canvas/Options/Blur")
-@onready var credits:Control = $Canvas/Options/Credits
+@onready var options:Control = $Menu/Options
+@onready var blackout:Control = $Blackout
+@onready var blur:Control = $Blur
 
-@onready var game_logotype:Control = $Canvas/MainMenu/Main/Buttons/Logotype/Logotype
-@onready var version:Label = $Canvas/MainMenu/Main/Credits/Version
-@onready var background:ColorRect = $Canvas/MainMenu/Background
-@onready var blackout:Control = $Canvas/MainMenu/Blackout
-@onready var logo_anim:AnimationPlayer = $Canvas/MainMenu/AnimationLogotype
-@onready var str_anim:AnimationPlayer = $Canvas/MainMenu/Main/Credits/AnimationString
-@onready var countinue_button = $Canvas/MainMenu/Main/Buttons/ButtonsContainer/HBoxContainer/CountinueGameMaring/CountinueGameButton
-@onready var newgame_button = $Canvas/MainMenu/Main/Buttons/ButtonsContainer/HBoxContainer/NewGameMargin/NewGameButton
-@onready var options_button = $Canvas/MainMenu/Main/Buttons/ButtonsContainer/HBoxContainer/OptionsMargin/OptionsButton
-@onready var quit_button = $Canvas/MainMenu/Main/Buttons/ButtonsContainer/HBoxContainer/QuitMargin/QuitButton
-@onready var font:Font = load("res://assets/fonts/moloko.otf")
-
-var clicked:bool = true
-var credits_status:bool = false
-var version_string_state:bool
-var logotype_state:bool
+@onready var credits:Label = $MarginContainer/MainContainer/Content/Credits
+var clicked:bool = false
 
 func _ready():
-	var app_version = ProjectSettings.get_setting("application/config/version")
-	var copyright = "(C) Studio Miroro"
-	version.text = "v" + app_version + "\n" + copyright
-	change_button_state(false)
-	blackout.visible = true
-	await get_tree().create_timer(1.25).timeout
-	blackout.blackout(false, 4)
-	await get_tree().create_timer(0.25).timeout
-	clicked = false
+	blackout.blackout(false)
+	credits.text = "v" + ProjectSettings.get_setting("application/config/version") + "\n(C) Studio Miroro"
 
-func _input(event):
-	if event is InputEventMouseButton\
-	&& event.button_index == MOUSE_BUTTON_LEFT\
-	&& event.is_pressed()\
-	&& !blur.state\
-	&& !clicked\
-	&& credits_status:
-		credits.open()
+func _on_continue_button_pressed():
+	if !clicked:
+		if !blur.state:
+			blackout.blackout(true)
+			blackout.change_scene("res://levels/farm.tscn")
+			GameLoader.mode = true
+			GameLoader.start = false
 
-func change_button_state(state:bool) -> void:
-	countinue_button.buttons_state(state)
-	newgame_button.buttons_state(state)
-	options_button.buttons_state(state)
-	quit_button.buttons_state(state)
-	change_logo_state(state)
-	change_string_state(state)
+func _on_new_game_button_pressed():
+	if !clicked:
+		if !blur.state:
+			blackout.blackout(true)
+			blackout.change_scene("res://levels/farm.tscn")
+			GameLoader.mode = false
+			GameLoader.start = true
 
-func change_logo_state(state:bool) -> void:
-	match state:
-		true:
-			_logo_hide()
-		false:
-			_logo_show()
+func _on_options_button_pressed():
+	if !clicked:
+		blur.blur(true)
+		options.open()
 
-func change_string_state(state: bool) -> void:
-	match state:
-		true:
-			_version_string_hide()
-		false:
-			_version_string_show()
-
-func _logo_hide() -> void:
-	logo_anim.play("logotype_hide")
-	logotype_state = false
-
-func _logo_show() -> void:
-	logo_anim.play("logotype_show")
-	logotype_state = true
-
-func _version_string_hide() -> void:
-	str_anim.play("version_string_hide")
-	version_string_state = false
-
-func _version_string_show() -> void:
-	str_anim.play("version_string_show")
-	version_string_state = true
-
-func _check_logo_state() -> void:
-	game_logotype.visible = logotype_state
-
-func _check_string_state() -> void:
-	version.visible = version_string_state
-
-func _on_area_2d_mouse_entered():
-	credits_status = true
-
-func _on_area_2d_mouse_exited():
-	credits_status = false
+func _on_exit_button_pressed():
+	if !clicked:
+		blackout.blackout(true)
+		await get_tree().create_timer(1).timeout
+		get_tree().quit()
