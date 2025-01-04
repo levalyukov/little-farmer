94dea108434c54fbabcdb278804f05bd8863cc09
levalyukov <lev.alyukov@gmail.com>
2025-01-04 22:17:39 +0500

    Update stall.gd

--- assets/scripts/construction/buildings/stall.gd
+++ assets/scripts/construction/buildings/stall.gd
@@ -9,6 +9,8 @@ extends Node2D
 @onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var player:CharacterBody2D = get_node("/root/"+main+"/Player")
 @onready var grid:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid") 
+@onready var clock:Control = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Bars/Clock")
+@onready var canvas:Node = get_node("/root/"+main+"/ShadowManager")
 @onready var sprite:Sprite2D = $Sprite2D
 
 var level:int = 1
@@ -17,8 +19,25 @@ var object:Dictionary = {
 	1: {
 		"caption" = tr("animalstall.caption"),
 		"description" = tr("animalstall.description"),
-		"default" = load("res://assets/resources/buildings/stall/object_0.png"),
-		"hover" = load("res://assets/resources/buildings/stall/object_1.png"),
+		"shadow" = load("res://assets/resources/buildings/stall/level_1/shadow.png"),
+		"seasons" = {
+			"spring" = {
+				"default" = load("res://assets/resources/buildings/stall/level_1/spring/object_0.png"),
+				"hovered" = load("res://assets/resources/buildings/stall/level_1/spring/object_1.png"),
+			},
+			"summer" = {
+				"default" = load("res://assets/resources/buildings/stall/level_1/summer/object_0.png"),
+				"hovered" = load("res://assets/resources/buildings/stall/level_1/summer/object_1.png"),
+			},
+			"autumn" = {
+				"default" = load("res://assets/resources/buildings/stall/level_1/autumn/object_0.png"),
+				"hovered" = load("res://assets/resources/buildings/stall/level_1/autumn/object_1.png"),
+			},
+			"winter" = {
+				"default" = load("res://assets/resources/buildings/stall/level_1/winter/object_0.png"),
+				"hovered" = load("res://assets/resources/buildings/stall/level_1/winter/object_1.png"),
+			},
+		}
 	},
 }
 
@@ -26,21 +45,46 @@ func _ready():
 	update()
 
 func update():
-	if object.has(level):
-		if object[level].has("default"):
-			if sprite:
-				sprite.texture = object[level]["default"]
+	if clock:
+		if sprite:
+			if object.has(level):
+				if object[level].has("seasons"):
+					var season = clock.get_season()
+					if object[level]["seasons"].has(season):
+						if object[level]["seasons"][season].has("default"):
+							sprite.texture = object[level]["seasons"][season]["default"]
+						else:
+							pass
+					else:
+						pass
+				else:
+					data.debug("There is no key at index " + str(level) + ".", "error")
+			else:
+				data.debug("Index " + str(level) + " is not in the dictionary.", "error")
 		else:
-			data.debug("There is no key at index " + str(level) + ".", "error")
+			pass
 	else:
-		data.debug("Index " + str(level) + " is not in the dictionary.", "error")
+		pass
 
 func _change_sprite(type:bool):
 	if type:
 		var distance = round(global_position.distance_to(player.global_position))
 		if grid.mode == grid.modes.NOTHING and distance < building.max_distance:
 			if object.has(level):
-				_check_sprite("hover")
+				if object.has(level):
+					if object[level].has("seasons"):
+						var season = clock.get_season()
+						if object[level]["seasons"].has(season):
+							if object[level]["seasons"][season].has("hovered"):
+								sprite.texture = object[level]["seasons"][season]["hovered"]
+							else:
+								pass
+						else:
+							pass
+					else:
+						data.debug("There is no key at index " + str(level) + ".", "error")
+				else:
+					data.debug("Index " + str(level) + " is not in the dictionary.", "error")
 				var level_text = tr("object.level")
 				tip.tooltip(
 					str(object[level]["caption"]) + "\n" +
@@ -48,22 +92,22 @@ func _change_sprite(type:bool):
 					str(level_text) + str(level)
 					)
 	else:
-		_check_sprite("default")
+		if object.has(level):
+			if object[level].has("seasons"):
+				var season = clock.get_season()
+				if object[level]["seasons"].has(season):
+					if object[level]["seasons"][season].has("default"):
+						sprite.texture = object[level]["seasons"][season]["default"]
+					else:
+						pass
+				else:
+					pass
+			else:
+				data.debug("There is no key at index " + str(level) + ".", "error")
+		else:
+			data.debug("Index " + str(level) + " is not in the dictionary.", "error")
 		if tip:
 			tip.tooltip("")
-
-func _check_sprite(key:String):
-	if object.has(level):
-		if object[level].has(key):
-			if object[level][key] is CompressedTexture2D:
-				if sprite:
-					sprite.texture = object[level][key]
-			else:
-				data.debug("The specified sprite cannot be installed.", "error")
-		else:
-			data.debug("There is no key at index " + str(level) + ".", "error")
-	else:
-		data.debug("Index " + str(level) + " is not in the dictionary.", "error")
 
 func get_data() -> Dictionary:
 	if object.has(level):
