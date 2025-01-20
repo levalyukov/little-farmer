8ca31b6a819d7809bacef3d2a14d976e1d6afd96
levalyukov <lev.alyukov@gmail.com>
2025-01-20 22:19:54 +0500

    Update dialog_window.gd

--- assets/scripts/ui/dialog_window.gd
+++ assets/scripts/ui/dialog_window.gd
@@ -1,15 +1,39 @@
 extends Control
 
-@onready var mainLabel:Label = $MarginContainer/Panel/VBoxContainer/MainText/VBoxContainer/MarginContainer2/Label
-@onready var buttonContainer = 
+@onready var mainLabel = $MarginContainer/Panel/VBoxContainer/MainText/VBoxContainer/MarginContainer2/Label
+@onready var buttonContainer = $MarginContainer/Panel/VBoxContainer/Answers/MarginContainer/HBoxContainer
+@onready var animation:AnimationPlayer = $AnimationPlayer
+var opened:bool = false
+var visibled:bool = false
 
 func _ready():
-	dialogWindow('GoodBye, World!', ['Next', 'Exit'])
+	dialogWindow(
+		"Разбитая могила",
+		[
+			'Перед вами заброшенная могила, разрушенная временем.\n\nНа потрескавшейся могильной плите едва угадываются несколько букв имени покоящейся здесь женщины:\n\n - Тя..на Анна (200x — 20xx)', 
+			'Приблизившись, вы замечаете под плитой небольшой клочок бумаги. Похоже, это чья-то записка.',
+		], 
+		{
+			0:['* Подойти поближе *','* Отойти *'],
+			1:['* Поднять записку *'],
+		},
+		{
+			0:[1,0],
+			1:[2],
+		}
+		)
 
-func dialogWindow(mainText:String, buttonsCaption:Array[String]):
-	mainLabel.setDialogText(mainText)
-	if buttonsCaption.size() > 0:
-		for i in buttonsCaption:
-			var button = Button.new()
-			button.text = i
+func dialogWindow(npcCaption:String, mainContent:Array[String], buttonsCaption:Dictionary, buttonFunc:Dictionary) -> void:
+	if !opened:
+		opened = true
+		visibled = true
+		animation.play('open')
+		mainLabel.setDialogText(npcCaption, mainContent, buttonsCaption, buttonFunc)
 
+func dialogWindowClose() -> void:
+	if opened:
+		animation.play('close')
+		visibled = !true
+
+func _check_window_state() -> void:
+	visible = visibled
