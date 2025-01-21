15bf845185562419a649cdce980426a9d28653db
levalyukov <lev.alyukov@gmail.com>
2025-01-21 19:20:52 +0500

    Update traders.gd

--- assets/scripts/ui/interactive/trading/traders.gd
+++ assets/scripts/ui/interactive/trading/traders.gd
@@ -4,65 +4,20 @@ class_name Traders
 var content:Dictionary = {
     1: {
         "inventory" = {
-            1:{
-                "amount": 25, 
-            },
-            2:{
-                "amount": 25, 
-            },
-            3:{
-                "amount": 25,
-            },
-            4:{
-                "amount": 25,     
-            },
-            5:{
-                "amount": 25,    
-            },
-            6:{
-                "amount": 25,
-            },
-            7:{
-                "amount": 25,
-            },
-            8:{
-                "amount": 25, 
-            },
-            9:{
-                "amount": 25,
-            },
-            10:{
-                "amount": 25, 
-            },
-            11:{
-                "amount": 25,
-            },
-            12:{
-                "amount": 25,
-            },
-            13:{
-                "amount": 250,
-            },
-            14:{
-                "amount": 250,
-            },
-            15:{
-                "amount": 250,
-            },
-            16:{
-                "amount": 250,
-            },
-            17:{
-                "amount": 250,
-            },
             18:{
-                "amount": 250,  
+                "amount": 25,
             },
             19:{
-                "amount": 250,
+                "amount": 25,
             },
             20:{
-                "amount": 250,
+                "amount": 25,
+            },
+            21:{
+                "amount": 25,
+            },
+            22:{
+                "amount": 25,
             },
         }
     }
