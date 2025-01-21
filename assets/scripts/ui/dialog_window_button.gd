302e1928fc62fc7d9104d2212eb0436a25c71b4d
levalyukov <lev.alyukov@gmail.com>
2025-01-21 19:21:33 +0500

    Update dialog_window_button.gd

--- assets/scripts/ui/dialog_window_button.gd
+++ assets/scripts/ui/dialog_window_button.gd
@@ -1,8 +1,10 @@
 extends Button
 
 @onready var main:String = str(get_tree().root.get_child(1).name)
-@onready var dialogWindow:Control = get_node('/root/'+main+'/')
-@onready var mainLabel:Label = get_node('/root/'+main+'/MarginContainer/Panel/VBoxContainer/MainText/VBoxContainer/MarginContainer2/Label')
+@onready var dialogWindow:Control = get_node('/root/'+main+'/UI/Interactive/DialogWindow/')
+@onready var mainLabel:Label = get_node('/root/'+main+'/UI/Interactive/DialogWindow/MarginContainer/Panel/VBoxContainer/MainText/VBoxContainer/MarginContainer2/Label')
+@onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
+@onready var trade:Control = get_node('/root/'+main+'/UI/Interactive/TradeMenu')
 
 var type:int
 enum TYPES {EXIT, NEXT, OPEN_TRADE, OPEN_NOTE}
@@ -11,11 +13,14 @@ func _on_pressed():
 	match type:
 		TYPES.EXIT:
 			dialogWindow.dialogWindowClose()
+			if blur:
+				blur.blur(false)
 		TYPES.NEXT:
 			mainLabel.nextCaption()
 		TYPES.OPEN_NOTE:
 			pass
 		TYPES.OPEN_TRADE:
-			pass
+			dialogWindow.dialogWindowClose()
+			trade.open_trade_menu()
 		_:
 			return
