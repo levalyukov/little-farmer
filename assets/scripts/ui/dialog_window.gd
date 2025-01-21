f52d6353581835be82cb97007045ab2647c4a24c
levalyukov <lev.alyukov@gmail.com>
2025-01-21 19:21:32 +0500

    Update dialog_window.gd

--- assets/scripts/ui/dialog_window.gd
+++ assets/scripts/ui/dialog_window.gd
@@ -1,27 +1,54 @@
 extends Control
 
-@onready var mainLabel = $MarginContainer/Panel/VBoxContainer/MainText/VBoxContainer/MarginContainer2/Label
-@onready var buttonContainer = $MarginContainer/Panel/VBoxContainer/Answers/MarginContainer/HBoxContainer
+@onready var mainLabel = $MarginContainer/Panel/VBoxContainer/MainContent/Panel/MainText/VBoxContainer/MarginContainer2/Label
+@onready var buttonContainer = $MarginContainer/Panel/VBoxContainer/MarginContainer2/Answers/MarginContainer/HBoxContainer
 @onready var animation:AnimationPlayer = $AnimationPlayer
+
+@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
+@onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
+
 var opened:bool = false
 var visibled:bool = false
 
 func _ready():
-	dialogWindow(
-		"Разбитая могила",
-		[
-			'Перед вами заброшенная могила, разрушенная временем.\n\nНа потрескавшейся могильной плите едва угадываются несколько букв имени покоящейся здесь женщины:\n\n - Тя..на Анна (200x — 20xx)', 
-			'Приблизившись, вы замечаете под плитой небольшой клочок бумаги. Похоже, это чья-то записка.',
-		], 
-		{
-			0:['* Подойти поближе *','* Отойти *'],
-			1:['* Поднять записку *'],
-		},
-		{
-			0:[1,0],
-			1:[2],
-		}
-		)
+	dialogWindowClose()
+
+func _input(event):
+	if Input.is_action_just_pressed('space'):
+		#dialogWindow(
+		#	"Разбитая могила",
+		#	[
+		#		'Перед вами заброшенная могила, разрушенная временем.\n\nНа потрескавшейся могильной плите едва угадываются несколько букв имени покоящейся здесь женщины:\n\n - Тя..на Анна (200x — 20xx)', 
+		#		'Приблизившись, вы замечаете под плитой небольшой клочок бумаги. Похоже, это чья-то записка.',
+		#	], 
+		#	{
+		#		0:['* Подойти поближе *','* Отойти *'],
+		#		1:['* Поднять записку *'],
+		#	},
+		#	{
+		#		0:[1,0],
+		#		1:[0],
+		#	}
+		#	)
+		dialogWindow(
+			'Добрыня',
+			[
+				'Добрый день, юный садовод! Меня зовут Добрыня — местный торговец и садовод по совместительству.\n\nУ меня ты можешь приобрести семена на сезон, а также полезные вещи для сада.', 
+			], 
+			{
+				0:['Интересно узнать твой ассортимент [Торговля]','Всего доброго! [Закрыть]'],
+			},
+			{
+				0:[2,0],
+			}
+			)
+
+	if Input.is_action_just_pressed("esc")\
+	&& blur.state\
+	&& !pause.paused\
+	&& opened:
+		dialogWindowClose()
 
 func dialogWindow(npcCaption:String, mainContent:Array[String], buttonsCaption:Dictionary, buttonFunc:Dictionary) -> void:
 	if !opened:
@@ -29,11 +56,16 @@ func dialogWindow(npcCaption:String, mainContent:Array[String], buttonsCaption:D
 		visibled = true
 		animation.play('open')
 		mainLabel.setDialogText(npcCaption, mainContent, buttonsCaption, buttonFunc)
+		if blur:
+			blur.blur(true)
 
 func dialogWindowClose() -> void:
 	if opened:
 		animation.play('close')
+		opened = !true
 		visibled = !true
 
 func _check_window_state() -> void:
 	visible = visibled
+	if pause:
+		pause.other_menu = opened
