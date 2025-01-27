4477f031af4d3d2afdca96cf63121a471c76426a
levalyukov <lev.alyukov@gmail.com>
2025-01-27 22:21:51 +0500

    Update gamedata.gd

--- assets/data/gamedata.gd
+++ assets/data/gamedata.gd
@@ -63,20 +63,19 @@ const sceneConfig = {
 }
 
 func _ready():
-	pass
-	#if main == "Farm":
-	#	if GameLoader.mode\
-	#	&& !GameLoader.start:
-	#		gameload()
-	#		GameLoader.mode = false
-	#	if !GameLoader.mode\
-	#	&& GameLoader.start:
-	#		nature.create_start_nature()
-	#else:
-	#	load_time()
-	#	load_balance()
-	#	load_inventory()
-	#	load_buildings()
+	if main == "Farm":
+		if GameLoader.mode\
+		&& !GameLoader.start:
+			gameload()
+			GameLoader.mode = false
+		if !GameLoader.mode\
+		&& GameLoader.start:
+			nature.create_start_nature()
+	else:
+		load_time()
+		load_balance()
+		load_inventory()
+		load_buildings()
 
 func gamesave() -> void:
 	file_save([path.main], file.config, get_dictionary_content("config"))
