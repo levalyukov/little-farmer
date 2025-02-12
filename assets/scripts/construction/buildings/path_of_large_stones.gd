84143299a75dfb84397cb2774a379402f10b7e90
levalyukov <lev.alyukov@gmail.com>
2025-02-12 22:31:03 +0500

    Update path_of_large_stones.gd

--- assets/scripts/construction/buildings/path_of_large_stones.gd
+++ assets/scripts/construction/buildings/path_of_large_stones.gd
@@ -1,3 +1,14 @@
 extends Node2D
 
+@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
+@onready var canvas:Node = get_node("/root/"+main+"/ShadowManager")
 var blueprint_id:int = 0
+
+func _ready():
+    if self:
+        if blueprint_id == 0:
+            var shadow = load("res://assets/resources/buildings/path_of_large_stones/shadow.png")
+            if shadow:
+                if shadow is CompressedTexture2D:
+                    canvas.create_shadow("path_of_large_stones_shadow", shadow, tilemap.local_to_map(self.position))
