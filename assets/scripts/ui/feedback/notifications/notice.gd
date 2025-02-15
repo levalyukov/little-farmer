6e7a7646f179377d4845c1a93cc1c4b0a7530cd1
levalyukov <lev.alyukov@gmail.com>
2025-02-15 20:22:29 +0500

    Update notice.gd

--- assets/scripts/ui/feedback/notifications/notice.gd
+++ assets/scripts/ui/feedback/notifications/notice.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main = str(get_tree().root.get_child(1).name)
+@onready var main = GameData.main
 @onready var data = get_node("/root/"+main)
 @onready var margin:MarginContainer = $Main
 @onready var icon:TextureRect = $Main/Content/HBoxContainer/MarginIcon/Icon
