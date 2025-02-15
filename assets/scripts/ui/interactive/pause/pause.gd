ef0e317694c01459c3006d09e57a9919d83c1e2b
levalyukov <lev.alyukov@gmail.com>
2025-02-15 20:23:38 +0500

    Update pause.gd

--- assets/scripts/ui/interactive/pause/pause.gd
+++ assets/scripts/ui/interactive/pause/pause.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main = str(get_tree().root.get_child(1).name)
+@onready var main = GameData.main
 @onready var data:Node2D = get_node("/root/"+main)
 @onready var ui:CanvasLayer = get_node("/root/"+main+"/UI")
 @onready var hud:Control = get_node("/root/"+main+"/UI/HUD/GameHud")
