04dda730620121a9bb85187c224969fd2df56969
levalyukov <lev.alyukov@gmail.com>
2025-02-15 20:23:26 +0500

    Update mail.gd

--- assets/scripts/ui/interactive/mail/mail.gd
+++ assets/scripts/ui/interactive/mail/mail.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var main:String = GameData.main
 @onready var data:Node2D = get_node("/root/"+main)
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var modal:Control = get_node("/root/"+main+"/UI/Feedback/Modal")
