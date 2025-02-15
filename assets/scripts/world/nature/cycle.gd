73d0d7fa99414cb5ffb130b3905da71167f0b5e5
levalyukov <lev.alyukov@gmail.com>
2025-02-15 20:23:44 +0500

    Update cycle.gd

--- assets/scripts/world/nature/cycle.gd
+++ assets/scripts/world/nature/cycle.gd
@@ -1,6 +1,6 @@
 extends CanvasModulate
 
-@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var main:String = GameData.main
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var blackout:Control = get_node("/root/"+main+"/UI/Decorative/Blackout")
 @onready var clock:Control = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Bars/Clock")
@@ -34,34 +34,34 @@ var value:float
 #               return
 
 func _process(delta):
-    if !pause.paused:
-        time_passed += (delta / real_seconds_per_game_minute)
-        time_passed = fmod(time_passed, game_day_duration)
-        cycle()
+	if !pause.paused:
+		time_passed += (delta / real_seconds_per_game_minute)
+		time_passed = fmod(time_passed, game_day_duration)
+		cycle()
 
 func cycle() -> void:
-    if !pause.paused\
-    && !blackout.blackouted:
-        if gradient_texture && gradient_texture.gradient:
-            var shifted_progress:float
-            if time_passed >= day_start && time_passed < day_end:
-                shifted_progress = (time_passed - day_start) / (day_end - day_start)
-                value = 1.0 - shifted_progress
-            else:
-                if time_passed < day_start:
-                    shifted_progress = (time_passed + game_day_duration - day_end) / (day_start + game_day_duration - day_end)
-                else:
-                    shifted_progress = (time_passed - day_end) / (day_start + game_day_duration - day_end)
-                value = shifted_progress
+	if !pause.paused\
+	&& !blackout.blackouted:
+		if gradient_texture && gradient_texture.gradient:
+			var shifted_progress:float
+			if time_passed >= day_start && time_passed < day_end:
+				shifted_progress = (time_passed - day_start) / (day_end - day_start)
+				value = 1.0 - shifted_progress
+			else:
+				if time_passed < day_start:
+					shifted_progress = (time_passed + game_day_duration - day_end) / (day_start + game_day_duration - day_end)
+				else:
+					shifted_progress = (time_passed - day_end) / (day_start + game_day_duration - day_end)
+				value = shifted_progress
 
-            if time_passed >= day_start && time_passed < day_end:
-                value = (sin(value * PI) + 1.0) / 2.0
-            else:
-                value = (1.0 - sin(value * PI)) / 2.0
-            color = gradient_texture.gradient.sample(value)
+			if time_passed >= day_start && time_passed < day_end:
+				value = (sin(value * PI) + 1.0) / 2.0
+			else:
+				value = (1.0 - sin(value * PI)) / 2.0
+			color = gradient_texture.gradient.sample(value)
 			
 func set_cycle_value(passed:float) -> void:
-    time_passed = passed * 60.0
+	time_passed = passed * 60.0
 
 func get_cycle_value() -> float:
-    return value
+	return value
