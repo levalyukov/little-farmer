50ef95087388a57feff2700d204a7122b490cdeb
levalyukov <lev.alyukov@gmail.com>
2025-02-15 20:22:30 +0500

    Update cursor.gd

--- assets/scripts/ui/hud/cursor.gd
+++ assets/scripts/ui/hud/cursor.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var main:String = GameData.main
 @onready var tooltip:Control = get_node("/root/"+main+"/UI/Feedback/Tooltip")
 
 enum states {DEFAULT}
