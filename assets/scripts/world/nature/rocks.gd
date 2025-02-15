b14eebbb538c6d119de43742efbb265958afd287
levalyukov <lev.alyukov@gmail.com>
2025-02-15 20:24:22 +0500

    Update rocks.gd

--- assets/scripts/world/nature/rocks.gd
+++ assets/scripts/world/nature/rocks.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var main:String = GameData.main
 @onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var canvas:Node = get_node("/root/"+main+"/ShadowManager")
 @onready var clock:Control = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Bars/Clock")
