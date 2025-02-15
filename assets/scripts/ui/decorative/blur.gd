bb71caa4c4635ef697d42c5c3307b93807b8c1c7
levalyukov <lev.alyukov@gmail.com>
2025-02-15 20:22:23 +0500

    Update blur.gd

--- assets/scripts/ui/decorative/blur.gd
+++ assets/scripts/ui/decorative/blur.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main:String = str(get_tree().root.get_child(1).name)
+@onready var main:String = GameData.main
 @onready var data = get_node("/root/"+main)
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var hud:Control = get_node("/root/"+main+"/UI/HUD/GameHud")
