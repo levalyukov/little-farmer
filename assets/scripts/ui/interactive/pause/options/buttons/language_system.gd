bbede43c4324ad7914baf2b96af590275cf193cc
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:30:30 +0500

    Update language_system.gd

--- assets/scripts/ui/interactive/pause/options/buttons/language_system.gd
+++ assets/scripts/ui/interactive/pause/options/buttons/language_system.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main = GameData.main
+@onready var main = str(get_tree().root.get_child(2).name)
 @onready var data:Node = get_node("/root/"+main)
 @export var languages:Array[String] = ["ru", "en"]
 var lang:int = 1
