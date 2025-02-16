7277fc600bd97140f03bf431d0c4cf91127ac0bc
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:29:54 +0500

    Update scene_caption.gd

--- assets/scripts/ui/hud/elements/scene_caption.gd
+++ assets/scripts/ui/hud/elements/scene_caption.gd
@@ -1,6 +1,6 @@
 extends MarginContainer
 
-@onready var main = GameData.main
+@onready var main = str(get_tree().root.get_child(2).name)
 @onready var data = get_node("/root/"+main)
 @onready var anim:AnimationPlayer = $AnimationPlayer
 @onready var label:Label = $Label
