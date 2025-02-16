b2be0217e25d7c760d79e0e75fac908fe165819e
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:30:38 +0500

    Update trade_system.gd

--- assets/scripts/ui/interactive/trading/trade_system.gd
+++ assets/scripts/ui/interactive/trading/trade_system.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main = GameData.main
+@onready var main = str(get_tree().root.get_child(2).name)
 @onready var data = get_node("/root/"+main)
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
