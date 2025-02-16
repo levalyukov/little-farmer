599f17afd64c670eda32b611cbebab91d765dcc1
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:28:18 +0500

    Update gameconfig.gd

--- assets/data/gameconfig.gd
+++ assets/data/gameconfig.gd
@@ -7,3 +7,11 @@ var fps_limit:bool = true
 var general:int = 100
 var music:int = 23
 var nature:int = 50
+
+func test():
+    print("vsync: ", vsync)
+    print("fullscreen: ", fullscreen)
+    print("fps_limit: ", fps_limit)
+    print("general: ", general)
+    print("music: ", music)
+    print("nature: ", nature)
