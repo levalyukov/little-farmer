0b9425b389bb537c88a0b6d54aa0ad0c88fef8aa
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:29:57 +0500

    Update collision.gd

--- assets/scripts/ui/hud/grid/collision.gd
+++ assets/scripts/ui/hud/grid/collision.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var main = GameData.main
+@onready var main = str(get_tree().root.get_child(2).name)
 @onready var data = get_node("/root/"+main)
 @onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var nature:Node2D = get_node("/root/"+main+"/Nature")
