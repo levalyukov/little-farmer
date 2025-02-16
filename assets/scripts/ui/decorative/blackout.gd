cc19fdec0a4fa9d2f0e60583a2e81f34f0f63b4d
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:29:08 +0500

    Update blackout.gd

--- assets/scripts/ui/decorative/blackout.gd
+++ assets/scripts/ui/decorative/blackout.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main = GameData.main
+@onready var main = str(get_tree().root.get_child(2).name)
 @onready var data = get_node("/root/"+main)
 @onready var background:ColorRect = $ColorRect
 @onready var anim:AnimationPlayer = $Animation
