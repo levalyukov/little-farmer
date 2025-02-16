a6f3262ee64efa93f42be86081cebf0f1b407675
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:30:28 +0500

    Update button_exit.gd

--- assets/scripts/ui/interactive/pause/buttons/button_exit.gd
+++ assets/scripts/ui/interactive/pause/buttons/button_exit.gd
@@ -1,6 +1,6 @@
 extends Button
 
-@onready var main = GameData.main
+@onready var main = str(get_tree().root.get_child(2).name)
 @onready var data = get_node("/root/"+main)
 @onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
@@ -11,5 +11,6 @@ func _on_pressed() -> void:
 		if pause.paused:
 			blackout.blackout(true)
 			if main == "Farm":
-				data.gamesave()
+				if visible:
+					data.gamesave()
 			blackout.change_scene("res://levels/menu.tscn")
