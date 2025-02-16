6cf3e7b82ae338c6642bf379b8df00bf06225d05
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:30:42 +0500

    Update cycle.gd

--- assets/scripts/world/nature/cycle.gd
+++ assets/scripts/world/nature/cycle.gd
@@ -1,6 +1,6 @@
 extends CanvasModulate
 
-@onready var main:String = GameData.main
+@onready var main:String = str(get_tree().root.get_child(2).name)
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var blackout:Control = get_node("/root/"+main+"/UI/Decorative/Blackout")
 @onready var clock:Control = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Bars/Clock")
