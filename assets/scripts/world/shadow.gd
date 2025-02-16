5ec3a756057e2572080ca79081a7bf3c8db7ea74
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:30:48 +0500

    Update shadow.gd

--- assets/scripts/world/shadow.gd
+++ assets/scripts/world/shadow.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var main:String = GameData.main
+@onready var main:String = str(get_tree().root.get_child(2).name)
 @onready var data:Node2D = get_node("/root/"+main)
 @onready var player:CharacterBody2D = get_node("/root/"+main+"/Player")
 @onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
