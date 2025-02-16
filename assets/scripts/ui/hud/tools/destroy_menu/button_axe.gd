a455688bb50cff0bd18fe3bfdb668714eaa5276e
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:30:12 +0500

    Update button_axe.gd

--- assets/scripts/ui/hud/tools/destroy_menu/button_axe.gd
+++ assets/scripts/ui/hud/tools/destroy_menu/button_axe.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main:String = GameData.main
+@onready var main:String = str(get_tree().root.get_child(2).name)
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var tools:HBoxContainer = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Tools")
 @onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
