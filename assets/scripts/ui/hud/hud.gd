8d8939636f6689b2b83d8dc05dc8250857da0803
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:30:00 +0500

    Update hud.gd

--- assets/scripts/ui/hud/hud.gd
+++ assets/scripts/ui/hud/hud.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main:String = GameData.main
+@onready var main:String = str(get_tree().root.get_child(2).name)
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var inventory:Control = get_node("/root/"+main+"/UI/Interactive/Inventory")
 @onready var crafting:Control = get_node("/root/"+main+"/UI/Interactive/Crafting")
