f8cb0bc2f635f1fe1217f99de0602065a0a636d1
levalyukov <lev.alyukov@gmail.com>
2025-02-16 10:30:25 +0500

    Update letter.gd

--- assets/scripts/ui/interactive/mail/letter.gd
+++ assets/scripts/ui/interactive/mail/letter.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main = GameData.main
+@onready var main = str(get_tree().root.get_child(2).name)
 @onready var data = get_node("/root/"+main)
 @onready var mail:Control = get_node("/root/"+main+"/UI/Interactive/Mailbox")
 @onready var icon:TextureRect = $Button/HBoxContainer/MarginContainer/TextureRect
