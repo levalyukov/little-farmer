7885fde28f4a401fb1ebb7d1705aa34108c6b22b
levalyukov <lev.alyukov@gmail.com>
2025-02-17 20:53:55 +0500

    Update tooltip.gd

--- assets/scripts/ui/feedback/tooltip.gd
+++ assets/scripts/ui/feedback/tooltip.gd
@@ -2,7 +2,6 @@ extends Control
 
 @onready var container:MarginContainer = $Container
 @onready var label:Label = $Container/MarginContainer/Label
-@onready var panel:Panel = $Container/Panel
 
 var tip:bool
 
