e660d7c326915a8cec815f3f85851fbc7b58768a
levalyukov <lev.alyukov@gmail.com>
2025-02-17 20:52:07 +0500

    Update gameconfig.gd

--- assets/data/gameconfig.gd
+++ assets/data/gameconfig.gd
@@ -2,16 +2,28 @@ extends Node
 
 var vsync:bool = false
 var fullscreen:bool = true
-var fps_limit:bool = true
+var fps_limit:int = true
     
 var general:int = 100
 var music:int = 23
 var nature:int = 50
 
-func test():
-    print("vsync: ", vsync)
-    print("fullscreen: ", fullscreen)
-    print("fps_limit: ", fps_limit)
-    print("general: ", general)
-    print("music: ", music)
-    print("nature: ", nature)
+func apply():
+    # V-Sync
+    if vsync:
+        ProjectSettings.set_setting("display/window/vsync/vsync_mode", 1)
+    else:
+        ProjectSettings.set_setting("display/window/vsync/vsync_mode", 0)
+    # Full Screen Mode
+    if fullscreen:
+        get_window().mode = Window.MODE_FULLSCREEN
+    else:
+        get_window().mode = Window.MODE_WINDOWED
+    # FPS Limit
+    match fps_limit:
+        0:
+            Engine.max_fps = 30
+        1:
+            Engine.max_fps = 60
+        _:
+            Engine.max_fps = 0
