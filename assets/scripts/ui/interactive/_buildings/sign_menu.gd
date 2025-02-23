94c1aa6befb6bc8e82bbc596ed663e01df1b4794
levalyukov <lev.alyukov@gmail.com>
2025-02-23 09:32:29 +0500

    Update sign_menu.gd

--- assets/scripts/ui/interactive/_buildings/sign_menu.gd
+++ assets/scripts/ui/interactive/_buildings/sign_menu.gd
@@ -1,17 +1,30 @@
 extends Control
 
-@onready var main = str(get_tree().root.get_child(1).name)
-@onready var data = get_node("/root/"+main)
+@onready var main:String = str(get_tree().root.get_child(2).name)
+@onready var data:Node2D = get_node("/root/"+main)
 @onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var inventory:Control = get_node("/root/"+main+"/UI/Interactive/Inventory")
 @onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
-@onready var container:GridContainer = $Main/VBoxContainer/MarginContainer/ScrollContainer/GridContainer
+@onready var container:GridContainer = $Main/VBoxContainer/MarginContainer/MarginContainer/ScrollContainer/GridContainer
 @onready var header:Label = $Main/VBoxContainer/Header/Label
 @onready var anim:AnimationPlayer = $AnimationPlayer
 
 var sign_name
 var opened:bool = false
 var items = Items.new()
+
+var items_to_create = []
+var current_item_index = 0
+
+func _process(_delta) -> void:
+	if visible:
+		if items_to_create.size() > 0 && current_item_index < items_to_create.size():
+			for i in range(1):
+				if current_item_index < items_to_create.size():
+					_create_item(items_to_create[current_item_index])
+					current_item_index += 1
+				else:
+					break
 
 func _ready():
 	_set_header()
@@ -33,16 +46,19 @@ func _close() -> void:
 	opened = false
 
 func _set_header() -> void:
-	var header_:String = tr("signmenu.header")
+	var header_:String = tr("Выберите иконку")
 	header.text = header_ + ":"
+
+func _create_item(id) -> void:
+	var node = inventory.node
+	var slot = node.instantiate()
+	container.add_child(slot)
+	slot.set_data(id, 1)
 
 func _create_all_items() -> void:
 	for item in items.content:
 		if items.content.has(item):
-			var node = inventory.node
-			var slot = node.instantiate()
-			container.add_child(slot)
-			slot.set_data(item, 1)
+			items_to_create.append(item)
 
 func _remove_all_items() -> void:
 	for item in container.get_children():
