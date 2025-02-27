6dccf2a1512e6f7a178d2ac9a92f012ec80f92df
levalyukov <lev.alyukov@gmail.com>
2025-02-27 19:08:06 +0500

    Update fertilizer.gd

--- assets/scripts/farming/fertilizer.gd
+++ assets/scripts/farming/fertilizer.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var main:String = str(get_tree().root.get_child(2).name)
+@onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var sprite:Sprite2D = $Sprite2D
 
 var items:Object = Items.new()
@@ -9,14 +11,31 @@ func set_fertilizer(fertilizerId:int) -> void:
 	id = fertilizerId
 	match fertilizerId:
 		61:
-			sprite.set_rect(0, 0)
+			sprite.region_rect.position.x = 0
+			sprite.region_rect.position.y = 0
 		62:
-			sprite.set_rect(16, 0)
+			sprite.region_rect.position.x = 16
+			sprite.region_rect.position.y = 0
 		_:
 			pass
-
+	
 func get_data() -> Dictionary:
 	return {
 		"fertilizerID": id,
+		"region_rect.x": sprite.region_rect.position.x,
+		"region_rect.y": sprite.region_rect.position.y,
+		"position": tilemap.local_to_map(global_position),
 	}
-			
+
+func set_data(
+		fertilizerID:int, 
+		region_rect_x:int, 
+		region_rect_y:int, 
+		vector:Vector2i,
+		caption:String,
+	) -> void:
+	id = fertilizerID
+	sprite.region_rect.position.x = region_rect_x
+	sprite.region_rect.position.y = region_rect_y
+	self.name = caption
+	set_position(tilemap.map_to_local(vector))
