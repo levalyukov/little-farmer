3aae349a9bdaf452fbd1cba7eb342f62116fbd91
levalyukov <lev.alyukov@gmail.com>
2025-03-02 09:14:39 +0500

    emergency saving

--- assets/data/gamefiles.gd
+++ assets/data/gamefiles.gd
@@ -11,3 +11,19 @@ class Funcs:
 		var error = DirAccess.remove_absolute(file_path)
 		if error != OK:
 			push_error("Invalid error: " + str(error))
+
+	static func delete_file(path: String) -> bool:
+		var dir = DirAccess.open(path.get_base_dir())
+		if dir and dir.file_exists(path.get_file()):
+			return dir.remove(path.get_file())
+		else:
+			print("Target file does not exist: ", path)
+			return false
+
+	static func delete_folder(path: String) -> bool:
+		var dir = DirAccess.open(path.get_base_dir())
+		if dir and dir.dir_exists(path.get_file()):
+			return dir.remove_recursive(path.get_file())
+		else:
+			print("Target folder does not exist: ", path)
+			return false
--- assets/data/gameloader.gd
+++ assets/data/gameloader.gd
@@ -3,3 +3,4 @@ extends Node
 var start:bool = false
 var mode:bool = false
 var modal:bool = false
+var greenhouse_caption:String = "greenhouse_1"
