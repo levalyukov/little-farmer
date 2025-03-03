a0b647cc5fab20fef704b2d37a2617259d1d2f39
levalyukov <lev.alyukov@gmail.com>
2025-03-03 22:25:32 +0500

    Update traders.gd

--- assets/scripts/ui/interactive/trading/traders.gd
+++ assets/scripts/ui/interactive/trading/traders.gd
@@ -4,11 +4,36 @@ class_name Traders
 var content:Dictionary = {
     1: {
         "inventory" = {
-            13:{"amount": 25},
-            14:{"amount": 25},
-            15:{"amount": 25},
-            16:{"amount": 25},
-            17:{"amount": 25},
+            "seasons": {
+                "spring": {
+                    13:{"amount": 25},
+                    14:{"amount": 25},
+                    15:{"amount": 25},
+                    16:{"amount": 25},
+                    17:{"amount": 25},
+                },
+                "summer": {
+                    18:{"amount": 25},
+                    19:{"amount": 25},
+                    20:{"amount": 25},
+                    21:{"amount": 25},
+                    22:{"amount": 25},
+                },
+                "autumn": {
+                    23:{"amount": 25},
+                    24:{"amount": 25},
+                    25:{"amount": 25},
+                    26:{"amount": 25},
+                    27:{"amount": 25},
+                },
+                "winter": {
+                    28:{"amount": 25},
+                    29:{"amount": 25},
+                    30:{"amount": 25},
+                    31:{"amount": 25},
+                    32:{"amount": 25},
+                },
+            }
         }
     }
 }
