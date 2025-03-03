90192d862229f223edbb2ed4cf63092dbb91f95f
levalyukov <lev.alyukov@gmail.com>
2025-03-03 08:49:49 +0500

    Update well.gd

--- assets/scripts/construction/buildings/well.gd
+++ assets/scripts/construction/buildings/well.gd
@@ -162,6 +162,7 @@ func get_data() -> Dictionary:
 		return {
 			"id": blueprint_id,
 			"position": tilemap.local_to_map(position),
-			"level": level
+			"level": level,
+			'all_collisions': all_collisions
 		}
 	return {}
