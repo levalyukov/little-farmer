6114742ab438e3bd019721c8977c7de7166cbacf
levalyukov <lev.alyukov@gmail.com>
2025-03-08 16:48:54 +0500

    Update stone_oven.gd

--- assets/scripts/construction/buildings/stone_oven.gd
+++ assets/scripts/construction/buildings/stone_oven.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var main:String = str(get_tree().root.get_child(2).name)
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
 @onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
 @onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var canvas:Node = get_node("/root/"+main+"/ShadowManager")
@@ -18,11 +19,11 @@ var blueprint_id:int = 0
 var destroyMode:bool = false
 var all_collisions:Array[Vector2i] = []
 var level:int = 1
-var ore_id:int = 0
+var ore_id = 0
 var ore_amount:int = 0
-var fuel_id:int = 0
+var fuel_id = 0
 var fuel_amount:int = 0
-var ignot_id:int = 0
+var ignot_id = 0
 var ignot_amount:int = 0
 
 var stoneMenuOpen:bool = false
@@ -60,22 +61,51 @@ func _input(event):
 	&& buttonDestroy.destroyMode:
 		buildings.remove_node(self, all_collisions)
 
+func update() -> void:
+	if !inProcessed:
+		if object.has(level):
+			if object[level].has('default_idle'):
+				if object[level]['default_idle'] is CompressedTexture2D:
+					sprite.texture = object[level]['default_idle']
+	else:
+		if object.has(level):
+			if object[level].has('default_work'):
+				if object[level]['default_work'] is CompressedTexture2D:
+					sprite.texture = object[level]['default_work']
+
 func _process(_delta):
+	if pause.paused:
+		if particles.emitting:
+			if particles.speed_scale > 0.0:
+				particles.speed_scale = 0.0
+	else:
+		if particles.emitting:
+			if particles.speed_scale == 0.0:
+				particles.speed_scale = 0.5
+
 	if !isDone:
 		if inProcessed:
+			if !particles.emitting:
+				particles.emitting = true
+
 			if value_process >= 100.0:
-				value_process = 100.0
-				ignot_id = items.content[ore_id]['oven_result']
-				ignot_amount = ore_amount / 5
-				inProcessed = false
-				isDone = true
-				timer.stop()
-				ore_id = 0
-				ore_amount = 0
-				fuel_id = 0
-				fuel_amount = 0
-				if stoneOvenMenu.visible:
-					stoneOvenMenu.check_button_state()
+				if items.content.has(int(ore_id)):
+					value_process = 100.0
+					ignot_id = items.content[int(ore_id)]['oven_result']
+					ignot_amount = ore_amount / 5
+					inProcessed = false
+					isDone = true
+					timer.stop()
+					ore_id = null
+					ore_amount = 0
+					fuel_id = null
+					fuel_amount = 0
+					update()
+					if stoneOvenMenu.visible:
+						stoneOvenMenu.check_button_state()
+	else:
+		if particles.emitting:
+			particles.emitting = false
 
 func _on_area_2d_mouse_entered():
 	if !blur.state\
@@ -97,14 +127,15 @@ func _on_area_2d_mouse_entered():
 						str(object[level]['caption']) + "\n" + str(object[level]['description'])
 					)
 		else:
-			if object.has(level):
-				if object[level].has('hovered_work'):
-					if object[level]['hovered_work'] is CompressedTexture2D:
-						sprite.texture = object[level]['hovered_work']
-			if tip:
-				tip.tooltip(
-					str(object[level]['caption']) + "\n" + str(object[level]['description'])
-				)
+			if !buttonDestroy.destroyMode:
+				if object.has(level):
+					if object[level].has('hovered_work'):
+						if object[level]['hovered_work'] is CompressedTexture2D:
+							sprite.texture = object[level]['hovered_work']
+				if tip:
+					tip.tooltip(
+						str(object[level]['caption']) + "\n" + str(object[level]['description'])
+					)
 
 		if !stoneMenuOpen:
 			stoneMenuOpen = true
@@ -133,15 +164,27 @@ func start_melt(oreID:int, oreAmount:int, fuelID:int, fuelAmount:int) -> void:
 	ore_amount = oreAmount
 	fuel_amount = fuelAmount
 	inProcessed = true
+	timer.wait_time = 2.5
 	timer.start()
+	update()
 
 func _on_timer_timeout():
-	if value_process <= 100.0:
-		value_process += randf_range(0.1, 50.0)
+	if !pause.paused:
+		if value_process <= 100.0:
+			value_process += randf_range(0.01, 2.5)
 
 func get_data() -> Dictionary:
 	return {
 		"level": level,
 		"position": tilemap.local_to_map(position),
 		"id": blueprint_id,
+		'inProcessed': inProcessed,
+		'value': value_process,
+		'isDone': isDone,
+		'oreID': ore_id,
+		'oreAmount': ore_amount,
+		'fuelID': fuel_id,
+		'fuelAmount': fuel_amount,
+		'ignotID': ignot_id,
+		'ignotAmount': ignot_amount
 	}
