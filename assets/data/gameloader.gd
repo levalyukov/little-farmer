1e96f5f57537b0e016a4b9444f631cc73b2eda38
levalyukov <lev.alyukov@gmail.com>
2025-03-20 20:16:07 +0500

    Update gameloader.gd

--- assets/data/gameloader.gd
+++ assets/data/gameloader.gd
@@ -3,25 +3,29 @@ extends Node
 var start:bool = false
 var mode:bool = false
 var modal:bool = true
+var tracking_plants:bool = false
+
 var greenhouse_caption:String = "greenhouse_1"
 var farm_plants:Dictionary = {}
 var greenhouse_plants:Dictionary = {}
 var time_left:float = 0.0
 var timer = Timer.new()
 
-func timer_plant_start():
-    if farm_plants != {}:
-        print(timer)
-        if timer:
-            self.add_child(timer)
-            timer.wait_time = 1.0
-            timer.connect("timeout", Callable(self, "on_timer_timeout"))
-            timer.start()
-
-func timer_plant_stop():
+func timer_farm_plant_start():
     if timer:
-        timer.stop()
+        self.add_child(timer)
+        timer.name = "farm_timer"
+        timer.wait_time = 1.0
+        timer.connect("timeout", Callable(self, "on_timer_timeout").bind(timer))
+        time_left = 0.0
+        timer.start()
 
-func on_timer_timeout() -> void:
+func timer_farm_plant_stop():
+    if timer:
+        if timer.name == 'farm_timer':
+            timer.stop()
+            self.remove_child(timer)
+
+func on_timer_timeout(timer_) -> void:
     time_left += 5.0
-    print(time_left)
+    print(timer_.name, ': ', time_left)
