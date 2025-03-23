f5a0a51bdb698a7bc48d30de233b88d7e39ef06e
levalyukov <lev.alyukov@gmail.com>
2025-03-23 21:38:32 +0500

    Create blueprints_shop.gd

---
+++ assets/scripts/ui/blueprints_shop.gd
@@ -0,0 +1,413 @@
+extends Control
+
+@onready var main:String = str(get_tree().root.get_child(2).name)
+@onready var data = get_node("/root/"+main)
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
+@onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
+@onready var balance:Control = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Bars/Balance")
+@onready var construct:Control = get_node("/root/"+main+"/UI/Interactive/ConstructMenu")
+
+@onready var buttonAllBlueprints:Button = $Panel/NavMenu/ButtonAllBlueprints
+@onready var buttonNodes:Button = $Panel/NavMenu/ButtonBuildings
+@onready var buttonTerrain:Button = $Panel/NavMenu/ButtonLandscape
+@onready var buttonUpgrades:Button = $Panel/NavMenu/ButtonUpgrades
+
+@onready var blueprintsHeader:Label = $Panel/MarginContainer/HBoxContainer/BlueprintsInfo/NinePatchRect/MarginContainer/VBoxContainer/CaptionMargin/Caption
+@onready var blueprintsDescription:Label = $Panel/MarginContainer/HBoxContainer/BlueprintsInfo/NinePatchRect/MarginContainer/VBoxContainer/DescriptionMargin/Description
+@onready var blueprintsPrice:RichTextLabel = $Panel/MarginContainer/HBoxContainer/BlueprintsInfo/NinePatchRect/MarginContainer/VBoxContainer/PriceMargin/Price
+@onready var blueprintsBuy:Button = $Panel/MarginContainer/HBoxContainer/BlueprintsInfo/NinePatchRect/MarginContainer/VBoxContainer/BuyMargin/Button
+@onready var blueprintsContainer:GridContainer = $Panel/MarginContainer/HBoxContainer/BlueprintsMargin/ScrollContainer/MarginContainer/BlueprintsContainer
+@onready var blueprintsNode:PackedScene = load("res://assets/nodes/ui/interactive/construct/blueprint.tscn")
+@onready var anim:AnimationPlayer = $AnimationPlayer
+
+var headerString:String = tr('Меню покупки чертежей')
+var descriptionString:String = tr('Выберите в левом меню чертеж для покупки.')
+
+var blueprints:Object = BlueprintManager.new()
+var opened:bool = false
+
+var section:String = 'all'
+var slots_to_create_terrains = []
+var slots_to_create_nodes = []
+var slots_to_create_upgrade = []
+
+var slots_to_create_terrains_captions = []
+var slots_to_create_nodes_captions = []
+var slots_to_create_upgrade_captions = []
+
+var current_slot_index_terrains = 0
+var current_slot_index_nodes = 0
+var current_slot_index_upgrade = 0
+
+var blueprint_section:String = ""
+var blueprint_index:int = 0
+var blueprint_price:int = 0
+
+func _input(_event):
+	if Input.is_action_just_pressed('space'):
+		open()
+
+func _ready():
+	window()
+
+func _process(_delta) -> void:
+	if visible:
+		# nodes
+		if slots_to_create_nodes.size() > 0 && current_slot_index_nodes < slots_to_create_nodes.size():
+			for i in range(1):
+				if current_slot_index_nodes < slots_to_create_nodes.size():
+					var blueprint = blueprintsNode.instantiate()
+					blueprintsContainer.add_child(blueprint)
+					blueprint.set_data("nodes", slots_to_create_nodes[current_slot_index_nodes])
+					if check_construct_array('nodes', slots_to_create_nodes[current_slot_index_nodes]):
+						if blueprints.content['nodes'][slots_to_create_nodes[current_slot_index_nodes]].has('trade_info'):
+							if blueprints.content['nodes'][slots_to_create_nodes[current_slot_index_nodes]]['trade_info'].has('caption'):
+								blueprint.disabled_button(
+									true, 
+									blueprints.content['nodes'][slots_to_create_nodes[current_slot_index_nodes]]['trade_info']['caption'], 
+									'(Имеется)'
+									)
+					current_slot_index_nodes += 1
+				else:
+					break
+		# terrains
+		if slots_to_create_terrains.size() > 0 && current_slot_index_terrains < slots_to_create_terrains.size():
+			for i in range(1):
+				if current_slot_index_terrains < slots_to_create_terrains.size():
+					var blueprint = blueprintsNode.instantiate()
+					blueprintsContainer.add_child(blueprint)
+					blueprint.set_data("terrains", slots_to_create_terrains[current_slot_index_terrains])
+					if check_construct_array('terrains', slots_to_create_terrains[current_slot_index_terrains]):
+						if blueprints.content['terrains'][slots_to_create_terrains[current_slot_index_terrains]].has('trade_info'):
+							if blueprints.content['terrains'][slots_to_create_terrains[current_slot_index_terrains]]['trade_info'].has('caption'):
+								blueprint.disabled_button(
+									true, 
+									blueprints.content['terrains'][slots_to_create_terrains[current_slot_index_terrains]]['trade_info']['caption'], 
+									'(Имеется)'
+									)
+					current_slot_index_terrains += 1
+				else:
+					break
+		# upgrades
+		if slots_to_create_upgrade.size() > 0 && current_slot_index_upgrade < slots_to_create_upgrade.size():
+			for i in range(1):
+				if current_slot_index_upgrade < slots_to_create_upgrade.size():
+					if check_construct_array('ugprades', slots_to_create_upgrade[current_slot_index_upgrade]):
+						var blueprint = blueprintsNode.instantiate()
+						blueprintsContainer.add_child(blueprint)
+						blueprint.set_data("ugprades", slots_to_create_upgrade[current_slot_index_upgrade])
+						if check_construct_array('ugprades', slots_to_create_upgrade[current_slot_index_upgrade]):
+							if blueprints.content['terrains'][slots_to_create_upgrade[current_slot_index_upgrade]].has('trade_info'):
+								if blueprints.content['terrains'][slots_to_create_upgrade[current_slot_index_upgrade]]['trade_info'].has('caption'):
+									blueprint.disabled_button(
+										true, 
+										blueprints.content['terrains'][slots_to_create_upgrade[current_slot_index_upgrade]]['trade_info']['caption'], 
+										'(Имеется)'
+										)
+						current_slot_index_upgrade += 1
+				else:
+					break
+
+func open() -> void:
+	opened = true
+	anim.play('open')
+	blur.blur(true)
+	section = 'all'
+	set_start_info()
+	get_blueprints()
+	update_nav_menu()
+
+func close() -> void:
+	opened = !true
+	anim.play('close')
+	blur.blur(!true)
+	clear_blueprints_container()
+
+func window() -> void:
+	visible = opened
+	if pause:
+		pause.other_menu = opened
+
+func get_data(group, index) -> void:
+	if blueprints.content.has(group):
+		if blueprints.content[group].has(index):
+			blueprint_section = group
+			blueprint_index = index
+			if blueprints.content[group][index].has('trade_info'):
+				if blueprints.content[group][index]['trade_info'].has('caption'):
+					blueprintsHeader.text = blueprints.content[group][index]['trade_info']['caption']
+				if blueprints.content[group][index]['trade_info'].has('description'):
+					blueprintsDescription.text = blueprints.content[group][index]['trade_info']['description']
+				if blueprints.content[group][index]['trade_info'].has('price'):
+					if blueprints.content[group][index]['trade_info']['price'] > 0:
+						blueprint_price = blueprints.content[group][index]['trade_info']['price']
+						blueprintsPrice.text = tr("Цена чертежа") + ": " + "[color=#ffce5e]" + balance.format(blueprints.content[group][index]['trade_info']['price']) + "[/color]"
+					else:
+						blueprintsPrice.text = ''
+				else:
+					blueprintsPrice.text = ''
+			update_buy_button(group, index)
+
+func check_construct_array(group,index) -> bool:
+	if construct:
+		match group.to_lower():
+			'nodes':
+				if construct.node_blueprints != []:
+					if construct.node_blueprints.has(index):
+						return true
+			'terrains':
+				if construct.terrains_blueprints != []:
+					if construct.terrains_blueprints.has(index):
+						return true
+			'upgrades':
+				if construct.upgrade_blueprints != []:
+					if construct.upgrade_blueprints.has(index):
+						return true
+	return false
+
+func update_buy_button(group, index) -> void:
+	if !blueprintsBuy.visible:
+		blueprintsBuy.visible = true
+	if blueprints.content[group][index].has('trade_info'):
+		if blueprints.content[group][index]['trade_info'].has('price'):
+			if balance.money >= blueprints.content[group][index]['trade_info']['price']:
+				blueprintsBuy.text = tr('Приобрести')
+				blueprintsBuy.disabled = false
+			else:
+				blueprintsBuy.text = tr('Недостаточно средств')
+				blueprintsBuy.disabled = true
+
+func get_blueprints() -> void:
+	clear_blueprints_container()
+	match section:
+		'nodes':
+			current_slot_index_nodes = 0
+			slots_to_create_nodes = []
+			if blueprints.content.has('nodes'):
+				if blueprints.content['nodes'] != {}:
+					for i in blueprints.content['nodes']:
+						slots_to_create_nodes.append(i)
+		'terrains':
+			current_slot_index_terrains = 0
+			slots_to_create_terrains = []
+			if blueprints.content.has('terrains'):
+				if blueprints.content['terrains'] != {}:
+					for i in blueprints.content['terrains']:
+						slots_to_create_terrains.append(i)
+		'upgrades':
+			current_slot_index_upgrade = 0
+			current_slot_index_upgrade = []
+			if blueprints.content.has('upgrades'):
+				if blueprints.content['upgrades'] != {}:
+					for i in blueprints.content['upgrades']:
+						current_slot_index_upgrade.append(i)
+		_:
+			current_slot_index_nodes = 0
+			current_slot_index_terrains = 0
+			current_slot_index_upgrade = 0
+			slots_to_create_nodes = []
+			slots_to_create_terrains = []
+			slots_to_create_upgrade = []
+			slots_to_create_nodes_captions = []
+			slots_to_create_terrains_captions = []
+			slots_to_create_upgrade_captions = []
+			if blueprints.content.has('nodes'):
+				if blueprints.content['nodes'] != {}:
+					for i in blueprints.content['nodes']:
+						slots_to_create_nodes.append(i)
+						if blueprints.content['nodes'][i].has('trade_info'):
+							if blueprints.content['nodes'][i]['trade_info'].has('caption'):
+								slots_to_create_nodes_captions.append(
+									blueprints.content['nodes'][i]['trade_info']['caption']
+								)
+			if blueprints.content.has('terrains'):
+				if blueprints.content['terrains'] != {}:
+					for i in blueprints.content['terrains']:
+						slots_to_create_terrains.append(i)
+						if blueprints.content['terrains'][i].has('trade_info'):
+							if blueprints.content['terrains'][i]['trade_info'].has('caption'):
+								slots_to_create_terrains_captions.append(
+									blueprints.content['terrains'][i]['trade_info']['caption']
+								)
+
+			if blueprints.content.has('upgrades'):
+				if blueprints.content['upgrades'] != {}:
+					for i in blueprints.content['upgrades']:
+						slots_to_create_upgrade.append(i)
+						if blueprints.content['upgrades'][i].has('trade_info'):
+							if blueprints.content['upgrades'][i]['trade_info'].has('caption'):
+								slots_to_create_upgrade_captions.append(
+									blueprints.content['upgrades'][i]['trade_info']['caption']
+									)
+
+func clear_blueprints_container() -> void:
+	if blueprintsContainer.get_children().size() > 0:
+		for i in blueprintsContainer.get_children():
+			blueprintsContainer.remove_child(i)
+
+func update_nav_menu() -> void:
+	if blueprints.content['nodes'] != {}:
+		buttonNodes.visible = true
+	else:
+		buttonNodes.visible = !true
+	if blueprints.content['terrains'] != {}:
+		buttonTerrain.visible = true
+	else:
+		buttonTerrain.visible = !true
+	if blueprints.content['upgrades'] != {}:
+		buttonUpgrades.visible = true
+	else:
+		buttonUpgrades.visible = !true
+
+	buttonAllBlueprints.text = construct.buttonSelectAllBlueprints
+	buttonNodes.text = construct.buttonSelectNodes
+	buttonTerrain.text = construct.buttonSelectTerrains
+	buttonUpgrades.text = construct.buttonSelectUpgrades
+	match section:
+		'nodes':
+			buttonAllBlueprints.modulate = Color(1, 1, 1, 0.784)
+			buttonNodes.modulate = Color(1, 1, 1)
+			buttonTerrain.modulate = Color(1, 1, 1, 0.784)
+			buttonUpgrades.modulate = Color(1, 1, 1, 0.784)
+		'terrains':
+			buttonAllBlueprints.modulate = Color(1, 1, 1, 0.784)
+			buttonNodes.modulate = Color(1, 1, 1, 0.784)
+			buttonTerrain.modulate = Color(1, 1, 1)
+			buttonUpgrades.modulate = Color(1, 1, 1, 0.784)
+		'upgrades':
+			buttonAllBlueprints.modulate = Color(1, 1, 1, 0.784)
+			buttonNodes.modulate = Color(1, 1, 1, 0.784)
+			buttonTerrain.modulate = Color(1, 1, 1, 0.784)
+			buttonUpgrades.modulate = Color(1, 1, 1)
+		_:
+			buttonAllBlueprints.modulate = Color(1, 1, 1)
+			buttonNodes.modulate = Color(1, 1, 1, 0.784)
+			buttonTerrain.modulate = Color(1, 1, 1, 0.784)
+			buttonUpgrades.modulate = Color(1, 1, 1, 0.784)
+
+func set_start_info() -> void:
+	blueprintsHeader.text = headerString
+	blueprintsDescription.text = descriptionString
+	blueprintsPrice.text = ''
+	blueprintsBuy.visible = false
+
+func _on_close_button_pressed():
+	var audio = AudioStreamPlayer.new()
+	self.add_child(audio)
+	audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+	audio.stream = load('res://assets/sounds/ui/click.ogg')
+	audio.play()
+	close()
+
+func _on_close_button_mouse_entered():
+	var audio = AudioStreamPlayer.new()
+	self.add_child(audio)
+	audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+	audio.stream = load('res://assets/sounds/ui/hover.ogg')
+	audio.play()
+
+func _on_button_all_blueprints_pressed():
+	if section != 'all':
+		section = 'all'
+		update_nav_menu()
+		get_blueprints()
+		var audio = AudioStreamPlayer.new()
+		self.add_child(audio)
+		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+		audio.stream = load('res://assets/sounds/ui/click.ogg')
+		audio.play()
+
+func _on_button_all_blueprints_mouse_entered():
+	if section != 'all':
+		var audio = AudioStreamPlayer.new()
+		self.add_child(audio)
+		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+		audio.stream = load('res://assets/sounds/ui/hover.ogg')
+		audio.play()
+
+func _on_button_buildings_pressed():
+	if section != 'nodes':
+		section = 'nodes'
+		update_nav_menu()
+		get_blueprints()
+		var audio = AudioStreamPlayer.new()
+		self.add_child(audio)
+		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+		audio.stream = load('res://assets/sounds/ui/click.ogg')
+		audio.play()
+
+func _on_button_buildings_mouse_entered():
+	if section != 'nodes':
+		var audio = AudioStreamPlayer.new()
+		self.add_child(audio)
+		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+		audio.stream = load('res://assets/sounds/ui/hover.ogg')
+		audio.play()
+
+func _on_button_landscape_pressed():
+	if section != 'terrains':
+		section = 'terrains'
+		update_nav_menu()
+		get_blueprints()
+		var audio = AudioStreamPlayer.new()
+		self.add_child(audio)
+		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+		audio.stream = load('res://assets/sounds/ui/click.ogg')
+		audio.play()
+
+func _on_button_landscape_mouse_entered():
+	if section != 'terrains':
+		var audio = AudioStreamPlayer.new()
+		self.add_child(audio)
+		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+		audio.stream = load('res://assets/sounds/ui/hover.ogg')
+		audio.play()
+
+func _on_button_upgrades_pressed():
+	if section != 'upgrades':
+		section = 'upgrades'
+		update_nav_menu()
+		get_blueprints()
+		var audio = AudioStreamPlayer.new()
+		self.add_child(audio)
+		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+		audio.stream = load('res://assets/sounds/ui/click.ogg')
+		audio.play()
+
+func _on_button_upgrades_mouse_entered():
+	if section != 'upgrades':
+		var audio = AudioStreamPlayer.new()
+		self.add_child(audio)
+		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+		audio.stream = load('res://assets/sounds/ui/hover.ogg')
+		audio.play()
+
+func _on_button_pressed():
+	if blueprintsBuy.visible:
+		if !blueprintsBuy.disabled:
+			var audio = AudioStreamPlayer.new()
+			var buy_audio = AudioStreamPlayer.new()
+			self.add_child(audio)
+			self.add_child(buy_audio)
+			audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+			buy_audio.connect("finished", Callable(self, "_on_audio_finished").bind(buy_audio))
+			audio.stream = load('res://assets/sounds/ui/click.ogg')
+			buy_audio.stream = load('res://assets/sounds/ui/trade.ogg')
+			audio.play()
+			buy_audio.play()
+			construct.add_blueprint(blueprint_index, blueprint_section)
+			balance.remove_money(blueprint_price)
+			get_blueprints()
+			set_start_info()
+
+func _on_button_mouse_entered():
+	if blueprintsBuy.visible:
+		if !blueprintsBuy.disabled:
+			var audio = AudioStreamPlayer.new()
+			self.add_child(audio)
+			audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+			audio.stream = load('res://assets/sounds/ui/hover.ogg')
+			audio.play()
+
+func _on_audio_finished(audio) -> void:
+	audio.queue_free()
