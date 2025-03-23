d2136b89327d15c3c568afe76d9596f22299fbd1
levalyukov <lev.alyukov@gmail.com>
2025-03-23 21:38:35 +0500

    Update blueprint.gd

--- assets/scripts/ui/interactive/construct/blueprint.gd
+++ assets/scripts/ui/interactive/construct/blueprint.gd
@@ -1,10 +1,14 @@
 extends Control
 
-@onready var main_scene = str(get_tree().root.get_child(2).name)
-@onready var data:Node2D = get_node("/root/" + main_scene)
-@onready var construct:Control = get_node("/root/" + main_scene + "/UI/Interactive/ConstructMenu")
+@onready var main = str(get_tree().root.get_child(2).name)
+@onready var data = get_node("/root/"+main)
+@onready var construct:Control = get_node("/root/"+main+"/UI/Interactive/ConstructMenu")
+@onready var blueprintsShop:Control = get_node("/root/"+main+"/UI/Interactive/BlueprintsShop")
 @onready var icon:TextureRect = $Button/HBoxContainer/MarginContainer/Icon
 @onready var caption:Label = $Button/HBoxContainer/Caption
+@onready var button:Button = $Button
+
+const MAX_SYMBOLS:int = 16
 
 var index:int
 var section:String
@@ -24,13 +28,36 @@ func set_data(group:String, id:int) -> void:
 				data.debug("["+str(id)+"] "+"The object does not have the 'icon' key.", "warning")
 
 			if blueprints.content[group][id].has("caption"):
-				caption.text = str(blueprints.content[group][id]["caption"])
+				caption.text = blueprints.content[group][id]["caption"]
 			else:
 				data.debug("["+str(id)+"] "+"The 'caption' key has a non-string type.", "warning")
 
+func disabled_button(state:bool, alternative_name:String, additional_string:String):
+	button.disabled = state
+	if state:
+		caption.modulate = Color(1, 1, 1, 0.784)
+		icon.modulate = Color(1, 1, 1, 0.784)
+	else:
+		caption.modulate = Color(1, 1, 1)
+		icon.modulate = Color(1, 1, 1)
+
+	if alternative_name != "":
+		caption.text = alternative_name
+
+	if additional_string != "":
+		if len(caption.get_text()) > MAX_SYMBOLS:
+			caption.text = caption.text.substr(0,MAX_SYMBOLS) + "... " + additional_string
+		else:
+			caption.text = caption.text + " " + additional_string
+
 func _on_button_pressed() -> void:
 	if visible:
-		construct.get_data(section, index)
+		if construct:
+			if construct.visible:
+				construct.get_data(section, index)
+		if blueprintsShop:
+			if blueprintsShop.visible:
+				blueprintsShop.get_data(section, index)
 		var audio = AudioStreamPlayer.new()
 		self.add_child(audio)
 		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
@@ -39,11 +66,12 @@ func _on_button_pressed() -> void:
 
 func _on_button_mouse_entered():
 	if visible:
-		var audio = AudioStreamPlayer.new()
-		self.add_child(audio)
-		audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
-		audio.stream = load('res://assets/sounds/ui/hover.ogg')
-		audio.play()
+		if !button.disabled:
+			var audio = AudioStreamPlayer.new()
+			self.add_child(audio)
+			audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+			audio.stream = load('res://assets/sounds/ui/hover.ogg')
+			audio.play()
 
 func _on_audio_finished(node) -> void:
 	node.queue_free()
