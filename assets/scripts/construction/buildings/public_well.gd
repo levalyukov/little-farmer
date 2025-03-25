e205caaf4f353271d58669e3d8515467fec38cf0
levalyukov <lev.alyukov@gmail.com>
2025-03-25 20:10:09 +0500

    Create public_well.gd

---
+++ assets/scripts/construction/buildings/public_well.gd
@@ -0,0 +1,145 @@
+extends Node2D
+
+@onready var main:String = str(get_tree().root.get_child(2).name)
+@onready var data:Node2D = get_node("/root/"+main)
+@onready var blur:Control = get_node("/root/"+main+"/UI/Decorative/Blur")
+@onready var pause:Control = get_node("/root/"+main+"/UI/Interactive/Pause")
+@onready var inventory:Control = get_node("/root/"+main+"/UI/Interactive/Inventory")
+@onready var canvas:Node = get_node("/root/"+main+"/ShadowManager")
+@onready var tip:Control = get_node("/root/"+main+"/UI/Feedback/Tooltip")
+@onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
+@onready var building:Node2D = get_node("/root/"+main+"/ConstructionManager")
+@onready var grid:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid") 
+@onready var buildings:Node2D = get_node("/root/"+main+"/ConstructionManager")
+@onready var player:CharacterBody2D = get_node("/root/"+main+"/Player")
+@onready var clock:Control = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Bars/Clock")
+@onready var blueprintsShop:Control = get_node("/root/"+main+"/UI/Interactive/BlueprintsShop")
+@onready var tools:HBoxContainer = get_node("/root/"+main+"/UI/HUD/GameHud/Main/Tools")
+@onready var sprite:Sprite2D = $Sprite2D
+
+var audio = AudioStreamPlayer.new()
+var openedMenu:bool = false
+var object:Dictionary = {
+	"caption" = tr("Колодец общего пользования"),
+	"description" = tr("Позволяет наполнить лейку"),
+	"shadow" = load("res://assets/resources/buildings/public_well/shadow.png"),
+	"default" = load("res://assets/resources/buildings/public_well/obj_0.png"),
+	"hovered" = load("res://assets/resources/buildings/public_well/obj_1.png"),
+}
+
+func _ready():
+	update()
+	update_shadow()
+	self.add_child(audio)
+
+func _input(event):
+	if event is InputEventMouseButton\
+	&& event.button_index == MOUSE_BUTTON_LEFT\
+	&& event.is_pressed()\
+	&& !blur.state\
+	&& grid.mode == grid.modes.NOTHING\
+	&& openedMenu:
+		if tools:
+			if tools.water_can < tools.water_can_max:
+				tools.water_can = tools.water_can_max
+				if audio:
+					if !audio.is_playing():
+						audio.stream = load('res://assets/sounds/buildings/using_well.ogg')
+						audio.play()
+				if tip.visible:
+					tip.tooltip()
+					tip.tooltip(
+							str(object["caption"]) + "\n" +
+							str(object["description"])
+						)
+
+func update():
+	if clock:
+		if object.has("seasons"):
+			var season = clock.get_season()
+			if object["seasons"].has(season):
+				if object["seasons"][season].has("default"):
+					if object["seasons"][season]["default"] is CompressedTexture2D:
+						sprite.texture = object["seasons"][season]["default"]
+					else:
+						data.debug("'"+str(self.name) + "': 'default' is not a CompressedTexture2D.", "error")
+			else:
+				data.debug("'"+str(self.name) + "': There is no '" + str(season) + "' key in the 'seasons' group.", "error")
+		else:
+			if object.has("default"):
+				if object["default"] is CompressedTexture2D:
+					sprite.texture = object["default"]
+				else:
+					data.debug("'"+str(self.name) + "': 'default' is not a CompressedTexture2D.", "error")
+			else:
+				data.debug("'"+str(self.name) + "': There is no 'default' key.", "error")
+
+func update_shadow() -> void:
+	if visible:
+		if object.has("shadow"):
+			if object["shadow"] is CompressedTexture2D:
+				canvas.create_shadow("house_shadow", object["shadow"], tilemap.local_to_map(position))
+			else:
+				data.debug("'"+str(self.name) + "': It is not possible to create a game shadow of an object because the sprite is not of the 'CompressedTexture2D' type.", "error")
+
+func _on_area_2d_mouse_entered() -> void:
+	openedMenu = true
+	if !blur.state\
+	&& grid.mode == grid.modes.NOTHING:
+		var distance = round(global_position.distance_to(player.global_position))
+		if distance < building.max_distance:
+			if object.has("seasons"):
+				var season = clock.get_season()
+				if object["seasons"].has(season):
+					if object["seasons"][season].has("hovered"):
+						if object["seasons"][season]["hovered"] is CompressedTexture2D:
+							sprite.texture = object["seasons"][season]["hovered"]
+						else:
+							data.debug()
+					else:
+						data.debug()
+				else:
+					data.debug()
+			else:
+				if object.has("hovered"):
+					if object["hovered"] is CompressedTexture2D:
+						sprite.texture = object["hovered"]
+					else:
+						data.debug("'"+str(self.name) + "': 'hovered' is not a CompressedTexture2D.", "error")
+				else:
+					data.debug("'"+str(self.name) + "': There is no 'hovered' key.", "error")
+			if tools:
+				if tools.water_can < tools.water_can_max:
+					if !tip.visible:
+						tip.tooltip(
+								str(object["caption"]) + "\n" +
+								str(object["description"]) + '\n' +
+								"- Вы можете набрать воду в лейку."
+							)
+				else:
+					if !tip.visible:
+						tip.tooltip(
+								str(object["caption"]) + "\n" +
+								str(object["description"])
+						)
+			else:
+				if !tip.visible:
+					tip.tooltip(
+							str(object["caption"]) + "\n" +
+							str(object["description"])
+						)
+
+func _on_area_2d_mouse_exited() -> void:
+	openedMenu = !true
+	if object.has("seasons"):
+		var season = clock.get_season()
+		if object["seasons"].has(season):
+			if object["seasons"][season].has("default"):
+				if object["seasons"][season]["default"] is CompressedTexture2D:
+					sprite.texture = object["seasons"][season]["default"]
+	else:
+		if object.has('default'):
+			if object['default'] is CompressedTexture2D:
+				sprite.texture = object['default']
+	if tip:
+		tip.tooltip("")
