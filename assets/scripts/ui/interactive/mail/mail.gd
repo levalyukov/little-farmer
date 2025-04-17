28de7b093e718d7ef0750e15595653e3f6331977
levalyukov <lev.alyukov@gmail.com>
2025-04-17 09:37:45 +0500

    Update mail.gd

--- assets/scripts/ui/interactive/mail/mail.gd
+++ assets/scripts/ui/interactive/mail/mail.gd
@@ -84,6 +84,11 @@ func letter(header:String, description:String = "", author:String = "", money:in
 		if items != {}: check_all_keys(key, items)
 		GameLoader.mailbox_indicator = true
 		mailbox.check_indicator_state()
+		var _audio = AudioStreamPlayer.new()
+		self.add_child(_audio)
+		_audio.connect("finished", Callable(self, "_on_audio_finished").bind(_audio))
+		_audio.stream = load('res://assets/sounds/ui/new_letter.ogg')
+		_audio.play()
 
 func check_all_keys(id, dictionary:Dictionary) -> void:
 	for key in dictionary.keys():
