d9c62fb197879ed08c30b03ee0eb7b2cda7ae3fc
levalyukov <lev.alyukov@gmail.com>
2025-04-26 20:27:15 +0500

    Create language.gd

---
+++ assets/scripts/ui/interactive/pause/options/buttons/language.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@onready var main = str(get_tree().root.get_child(2).name)
+@onready var data:Node = get_node("/root/"+main)
+@export var languages:Array[String] = ["ru", "en"]
+var lang:int = 0
+
+func _ready():
+	set_language(lang)
+
+func change_language() -> void:
+	lang = (lang + 1) % languages.size()
+	TranslationServer.set_locale(languages[lang])
+	GameConfig.language = lang
+	if data.has_method("debug"):
+		data.debug("Game language changed: " + languages[lang])
+	else:
+		print("Game language changed: ", languages[lang])
+
+func set_language(value:int) -> void:
+	lang = value
+	TranslationServer.set_locale(languages[lang])
+
+func _on_pressed() -> void:
+	match main:
+		'MainMenu':
+			var options:Control = get_node("/root/"+main+"/Menu/Options")
+			if options.opened:
+				change_language()
+		_:
+			var options:Control = get_node("/root/"+main+"/UI/Interactive/Options")
+			if options.opened:
+				change_language()
