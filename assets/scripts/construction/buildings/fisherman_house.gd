bf9b6be71df0355fd952649eb08adb02ac64d599
levalyukov <lev.alyukov@gmail.com>
2025-05-05 20:00:38 +0500

    Update fisherman_house.gd

--- assets/scripts/construction/buildings/fisherman_house.gd
+++ assets/scripts/construction/buildings/fisherman_house.gd
@@ -23,7 +23,6 @@ const bakeEveninggOn:int = 19
 const bakeEveninggOff:int = 22
 #	var openedTradeMenu:bool = false
 var object:Dictionary = {
-	"caption" = tr("Рыбацкий дом Марлина."),
 	"shadow" = load("res://assets/resources/buildings/fisherman_house/shadow.png"),
 	"default" = load("res://assets/resources/buildings/fisherman_house/object_0.png"),
 	"hovered" = load("res://assets/resources/buildings/fisherman_house/object_1.png"),
@@ -117,7 +116,7 @@ func _on_area_2d_mouse_entered() -> void:
 						data.debug("'"+str(self.name) + "': 'hovered' is not a CompressedTexture2D.", "error")
 				else:
 					data.debug("'"+str(self.name) + "': There is no 'hovered' key.", "error")
-			tip.tooltip(str(object["caption"]))
+			tip.tooltip(tr("object.fisherman_house.caption"))
 
 func _on_area_2d_mouse_exited() -> void:
 	#	openedTradeMenu = !true
