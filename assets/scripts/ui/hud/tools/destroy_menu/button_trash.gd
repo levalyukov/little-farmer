40b6939dce05c55fe6aa385f08e046e93dffe67a
levalyukov <lev.alyukov@gmail.com>
2025-05-05 20:01:05 +0500

    Update button_trash.gd

--- assets/scripts/ui/hud/tools/destroy_menu/button_trash.gd
+++ assets/scripts/ui/hud/tools/destroy_menu/button_trash.gd
@@ -38,7 +38,7 @@ func _on_button_mouse_entered():
 			audio.stream = load('res://assets/sounds/ui/hover.ogg')
 			audio.play()
 			if cursor: cursor.set_cursor(cursor.states.ACTIVE)
-			if tip: tip.tooltip('Уничтожение ландшафта')
+			if tip: tip.tooltip(tr('ui.tools.terrain_destroy_button'))
 
 func _on_button_mouse_exited():
 	if cursor: cursor.set_cursor(cursor.states.DEFAULT)
