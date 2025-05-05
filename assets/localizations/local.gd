f005e954dae5f2a7e00f72db3c7d71247a8865d4
levalyukov <lev.alyukov@gmail.com>
2025-05-05 20:00:05 +0500

    Create local.gd

---
+++ assets/localizations/local.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _ready():
+	tr("letter.for_new_player.header")		# Письмо
+	tr("letter.for_new_player.content")		# Новому
+	tr("letter.for_new_player.signature")	# Игроку
+
+	tr('npc.dobrynya')	# Имена
+	tr('npc.kuzma')		# NPC
+	tr('npc.vance')		# 
