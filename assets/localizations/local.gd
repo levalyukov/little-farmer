87d872e100c163af8c55d2911d2dae395c98c154
levalyukov <lev.alyukov@gmail.com>
2025-05-05 22:03:46 +0500

    Update local.gd

--- assets/localizations/local.gd
+++ assets/localizations/local.gd
@@ -5,6 +5,17 @@ func _ready():
 	tr("letter.for_new_player.content")		# Новому
 	tr("letter.for_new_player.signature")	# Игроку
 
+	tr('letter.public_well_announcement_header')
+	tr('letter.public_well_announcement_content')
+	tr('letter.korney_korneich.signature')
+
+	tr('letter.gardener_dobrynya')
+	tr('letter.reminder_season_header')
+	tr('letter.reminder_season_description_spring_->_summer')
+	tr('letter.reminder_season_description_summer_->_autumn')
+	tr('letter.reminder_season_description_autumn_->_winter')
+	tr('letter.reminder_season_description_winter_->_spring')
+
 	tr('npc.dobrynya')	# Имена
 	tr('npc.kuzma')		# NPC
 	tr('npc.vance')		# 
