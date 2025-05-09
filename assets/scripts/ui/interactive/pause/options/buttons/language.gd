5c08dc212cffc8940afe09bc4f5b20effd137886
levalyukov <lev.alyukov@gmail.com>
2025-05-09 08:18:17 +0500

    Update language.gd

--- assets/scripts/ui/interactive/pause/options/buttons/language.gd
+++ assets/scripts/ui/interactive/pause/options/buttons/language.gd
@@ -18,6 +18,11 @@ func set_language(value:int) -> void:
 	TranslationServer.set_locale(languages[lang])
 
 func _on_pressed() -> void:
+	var audio = AudioStreamPlayer.new()
+	self.add_child(audio)
+	audio.connect("finished", Callable(self, "_on_audio_finished").bind(audio))
+	audio.stream = load('res://assets/sounds/ui/click.ogg')
+	audio.play()
 	match main:
 		'MainMenu':
 			var options:Control = get_node("/root/"+main+"/Menu/Options")
@@ -27,3 +32,24 @@ func _on_pressed() -> void:
 			var options:Control = get_node("/root/"+main+"/UI/Interactive/Options")
 			if options.opened:
 				change_language()
+
+func _on_mouse_entered():
+	match main:
+		'MainMenu':
+			var cursor = get_node('/root/'+main+'/Cursor')
+			if cursor: cursor.set_cursor(cursor.states.ACTIVE)
+		_:
+			var cursor:Node2D = get_node("/root/"+main+"/UI/HUD/Cursor")
+			if cursor: cursor.set_cursor(cursor.states.ACTIVE)
+
+func _on_mouse_exited():
+	match main:
+		'MainMenu':
+			var cursor = get_node('/root/'+main+'/Cursor')
+			if cursor: cursor.set_cursor(cursor.states.DEFAULT)
+		_:
+			var cursor:Node2D = get_node("/root/"+main+"/UI/HUD/Cursor")
+			if cursor: cursor.set_cursor(cursor.states.DEFAULT)
+
+func _on_audio_finished(node) -> void:
+	node.queue_free()
