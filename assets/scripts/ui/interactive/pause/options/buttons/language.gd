f10edd7baeb56b7c73b8b3707ade4eeef1d49720
levalyukov <lev.alyukov@gmail.com>
2025-05-11 19:56:31 +0500

    Update language.gd

--- assets/scripts/ui/interactive/pause/options/buttons/language.gd
+++ assets/scripts/ui/interactive/pause/options/buttons/language.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var main = str(get_tree().root.get_child(2).name)
 @onready var data:Node = get_node("/root/"+main)
 @export var languages:Array[String] = ["ru", "en"]
-var lang:int = 1
+var lang:int = 0
 
 func _ready():
 	set_language(lang)
