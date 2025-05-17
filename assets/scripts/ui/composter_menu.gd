62c21e4bfb5e72dc8aa1a19d7d0f4f941e7da2d3
levalyukov <lev.alyukov@gmail.com>
2025-05-17 12:11:34 +0500

    Update composter_menu.gd

--- assets/scripts/ui/composter_menu.gd
+++ assets/scripts/ui/composter_menu.gd
@@ -26,10 +26,14 @@ const MAX_COMPOST_TIME:int = 300
 const COMPOST_THRESHOLD:int = 4
 
 var current_slot_index:int = 0
-var slots_to_create:Array = []
 var current_node:Node2D
 var items:Object = Items.new()
 var opened:bool = false
+
+var items_id_to_create:Array = []
+var items_amount_to_create:Array = []
+var items_icons_to_create:Array = []
+var items_captions_to_create:Array = []
 
 func _ready():
 	close()
@@ -42,14 +46,20 @@ func _input(_event):
 		close()
 
 func _process(_delta) -> void:
-	if visible:
-		if slots_to_create.size() > 0 and current_slot_index < slots_to_create.size():
-			for i in range(1):
-				if current_slot_index < slots_to_create.size():
-					item_create(slots_to_create[current_slot_index])
-					current_slot_index += 1
-				else:
-					break
+	if visible && (items_id_to_create.size() > 0 && current_slot_index < items_id_to_create.size()):
+		for i in range(1):
+			if current_slot_index < items_id_to_create.size():
+				item_create(
+					items_id_to_create[current_slot_index],
+					items_amount_to_create[current_slot_index],
+					items_icons_to_create[current_slot_index],
+					items_captions_to_create[current_slot_index],
+					0,
+					playerInventoryItemsContainer
+					)
+				current_slot_index += 1
+			else:
+				break
 		if current_node:
 			if current_node.composting:
 				if current_node.composting_value < 100.0:
@@ -67,6 +77,8 @@ func _process(_delta) -> void:
 						getCompostButton.visible = true
 					if startComposting.visible:
 						startComposting.visible = false
+	else:
+		set_process(false)
 
 func add_item_compost(id, amount:int = 1) -> void:
 	if visible:
@@ -78,6 +90,8 @@ func add_item_compost(id, amount:int = 1) -> void:
 			else:
 				if current_node.compost_items[id]["amount"] + amount <= inventory.inventory_items[id]["amount"]:
 					current_node.compost_items[id]["amount"] += amount
+				else:
+					current_node.compost_items[id]["amount"] = inventory.inventory_items[id]["amount"]
 			update_compost_items()
 
 func remove_item_compost(id, amount:int = 1) -> void:
@@ -103,19 +117,24 @@ func update_compost_items() -> void:
 	if visible:
 		if current_node:
 			clear_compost_items()
-			for i in current_node.compost_items:
-				var slot = inventory.node.instantiate()
-				itemsForCompostContainer.add_child(slot)
-				slot.set_data(i, current_node.compost_items[i]["amount"])
-				slot.cmpst_type = 1
+			for item_id in current_node.compost_items:
+				var item_amount = current_node.compost_items[item_id]["amount"]
+				var item_icon = items.content[item_id]['icon']
+				var item_caption = items.content[item_id]['caption']
+				item_create(item_id, item_amount, item_icon, item_caption, 1, itemsForCompostContainer)
 
-func item_create(id) -> void:
+func item_create(item_id, item_amount, item_icon, item_caption, compost_type, container) -> void:
 	var slot = inventory.node.instantiate()
-	if inventory.inventory_items.has(id):
-		if inventory.inventory_items[id]["amount"] > 0:
-			playerInventoryItemsContainer.add_child(slot)
-			slot.set_data(id, inventory.inventory_items[id]["amount"])
-			slot.cmpst_type = 0
+	if inventory.inventory_items.has(item_id):
+		if inventory.inventory_items[item_id]["amount"] > 0:
+			container.add_child(slot)
+			slot.set_data(
+					item_id, 
+					item_amount,
+					item_icon,
+					item_caption
+				)
+			slot.compost_type = compost_type
 
 func get_compost_state() -> void:
 	if getCompostButton.visible && current_node.composting_value != 100.0:
@@ -136,19 +155,28 @@ func check_state_button() -> void:
 
 func get_compost_items() -> void:
 	remove_all_inventory_items()
-	slots_to_create = []
-	for item in inventory.inventory_items:
-		if items.content.has(int(item)):
-			if items.content[int(item)].has("item_type"):
-				if items.content[int(item)]["item_type"] == "compost":
-					if inventory.inventory_items[item].has("amount"):
-						if inventory.inventory_items[item]["amount"] > 0:
-							slots_to_create.append(item)
+	items_id_to_create = []
+	items_amount_to_create = []
+	for item_id in inventory.inventory_items:
+		if items.content.has(int(item_id)):
+			if items.content[int(item_id)].has("item_type"):
+				if items.content[int(item_id)]["item_type"] == "compost":
+					if inventory.inventory_items[item_id].has("amount"):
+						var item_icon = items.content[int(item_id)]["icon"]
+						var item_caption = items.content[int(item_id)]["caption"]
+						var item_amount = inventory.inventory_items[item_id]["amount"]
+						if inventory.inventory_items[item_id]["amount"] > 0:
+							items_id_to_create.append(item_id)
+							items_icons_to_create.append(item_icon)
+							items_captions_to_create.append(item_caption)
+							items_amount_to_create.append(item_amount)
 	current_slot_index = 0
+	set_process(true)
 
 func remove_all_inventory_items() -> void:
-	for i in playerInventoryItemsContainer.get_children():
-		playerInventoryItemsContainer.remove_child(i)
+	for node in playerInventoryItemsContainer.get_children():
+		playerInventoryItemsContainer.remove_child(node)
+		node.queue_free()
 
 func check_all_states() -> void:
 	if !current_node.composting:
@@ -166,6 +194,7 @@ func open(node:Node2D) -> void:
 	opened = true
 	blur.blur(true)
 	anim.play("open")
+	_check_window()
 	get_compost_state()
 	get_compost_items()
 	clear_compost_items()
@@ -205,11 +234,7 @@ func _on_get_compost_button_pressed() -> void:
 	check_state_button()
 	current_node.update_texture()
 	compostingProcessLabel.text = tr("composting_menu.description")
-	var _audio = AudioStreamPlayer.new()
-	self.add_child(_audio)
-	_audio.connect("finished", Callable(self, "_on_audio_finished").bind(_audio))
-	_audio.stream = load('res://assets/sounds/ui/click.ogg')
-	_audio.play()
+	play_sound('ui/click')
 
 func check_items_count() -> bool:
 	for i in current_node.compost_items:
@@ -229,9 +254,9 @@ func get_items_count() -> int:
 
 func _on_turn_button_pressed() -> void:
 	if itemsForCompostContainer.get_children().size() > 0:
-		for i in current_node.compost_items:
+		for i in current_node.compost_items: 
 			inventory.subject_item(i, current_node.compost_items[i]["amount"])
-		if itemsForCompostContainer.get_children().size() >= 8:
+		if itemsForCompostContainer.get_children().size() >= 8: 
 			current_node.highQuality = true
 		current_node.start_compost(get_items_count())
 		current_node.update_texture()
@@ -242,49 +267,25 @@ func _on_turn_button_pressed() -> void:
 		current_node.composting = true
 		current_node.compost_items = {}
 		current_node.composting_value = 0.0
-		var _audio = AudioStreamPlayer.new()
-		self.add_child(_audio)
-		_audio.connect("finished", Callable(self, "_on_audio_finished").bind(_audio))
-		_audio.stream = load('res://assets/sounds/ui/click.ogg')
-		_audio.play()
+		play_sound('ui/click')
 
 func _on_button_exit_pressed() -> void:
-	var _audio = AudioStreamPlayer.new()
-	self.add_child(_audio)
-	_audio.connect("finished", Callable(self, "_on_audio_finished").bind(_audio))
-	_audio.stream = load('res://assets/sounds/ui/click.ogg')
-	_audio.play()
+	play_sound('ui/click')
 	close()
 
 func _on_button_exit_mouse_entered():
 	if cursor: cursor.set_cursor(cursor.states.ACTIVE)
-	var _audio = AudioStreamPlayer.new()
-	self.add_child(_audio)
-	_audio.connect("finished", Callable(self, "_on_audio_finished").bind(_audio))
-	_audio.stream = load('res://assets/sounds/ui/hover.ogg')
-	_audio.play()
-	
-func _on_audio_finished(node) -> void:
-	node.queue_free()
+	play_sound('ui/hover')
 
 func _on_get_compost_button_mouse_entered():
-	if getCompostButton.visible:
+	if getCompostButton.visible && getCompostButton.disabled:
 		if cursor: cursor.set_cursor(cursor.states.ACTIVE)
-		var _audio = AudioStreamPlayer.new()
-		self.add_child(_audio)
-		_audio.connect("finished", Callable(self, "_on_audio_finished").bind(_audio))
-		_audio.stream = load('res://assets/sounds/ui/hover.ogg')
-		_audio.play()
+		play_sound('ui/hover')
 
 func _on_turn_button_mouse_entered():
 	if startComposting.visible && !startComposting.disabled:
 		if cursor: cursor.set_cursor(cursor.states.ACTIVE)
-		var _audio = AudioStreamPlayer.new()
-		self.add_child(_audio)
-		_audio.connect("finished", Callable(self, "_on_audio_finished").bind(_audio))
-		_audio.stream = load('res://assets/sounds/ui/hover.ogg')
-		_audio.play()
-
+		play_sound('ui/hover')
 
 func _on_get_compost_button_mouse_exited():
 	if cursor: cursor.set_cursor(cursor.states.DEFAULT)
@@ -294,3 +295,13 @@ func _on_turn_button_mouse_exited():
 
 func _on_button_exit_mouse_exited():
 	if cursor: cursor.set_cursor(cursor.states.DEFAULT)
+
+func play_sound(ogg_name:String) -> void:
+	var ui_audio = AudioStreamPlayer.new()
+	self.add_child(ui_audio)
+	ui_audio.connect("finished", Callable(self, "_on_audio_finished").bind(ui_audio))
+	ui_audio.stream = load('res://assets/sounds/'+ogg_name+'.ogg')
+	ui_audio.play()
+
+func _on_audio_finished(node) -> void:
+	node.queue_free()
