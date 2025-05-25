de2f047e52be989182eba0438a5e1fdca311558a
levalyukov <lev.alyukov@gmail.com>
2025-05-25 11:18:31 +0500

    Update blur.gd

--- assets/scripts/ui/decorative/blur.gd
+++ assets/scripts/ui/decorative/blur.gd
@@ -9,23 +9,25 @@ extends Control
 @onready var zoom:Camera2D = get_node("/root/"+main+"/Player/Camera2D")
 @onready var background:ColorRect = $ColorRect
 
-const max_blur:float = 1.5
-const min_blur:float = 0.0
+const MAX:float = 1.5
+const MIN:float = 0.0
 const speed:float = 0.1
+const EPSILON:float = 0.001
 
 var value:float = 0
 var state:bool = false
 
 func _process(_delta):
-	if state:
-		if value < max_blur:
-			value = value + speed
-	else:
-		if value > min_blur:
-			value = value - speed
+	if state: value = min(value + speed, MAX)
+	else: value = max(value - speed, MIN)
 	background.material.set_shader_parameter("lod", value)
+	if state && abs(value - MAX) < EPSILON:
+		set_process(false)
+	elif !state && abs(value - MIN) < EPSILON:
+		set_process(false)
 
 func blur(bluring:bool) -> void:
+	set_process(true)
 	state = bluring
 	if has_node("/root/"+main+"/UI/HUD/GameHud"):
 		if state:
