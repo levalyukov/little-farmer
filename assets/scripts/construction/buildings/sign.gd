7bf2f51e3fda3058797a1900bc0cfafdf987a116
levalyukov <lev.alyukov@gmail.com>
2025-05-26 01:40:51 +0500

    Update sign.gd

--- assets/scripts/construction/buildings/sign.gd
+++ assets/scripts/construction/buildings/sign.gd
@@ -17,40 +17,37 @@ extends Node2D
 @onready var icon:TextureRect = $TextureRect
 @onready var sprite:Sprite2D = $Sprite2D
 
-const level:int = 0
+var items = Items.new()
 
 var destroyMode:bool = false
 var all_collisions:Array[Vector2i] = []
-var items = Items.new()
 var sprite_id:int = 0
 var blueprint_id:int = 0
 var max_distance:int = 250
 var open_menu:bool = false
 var vector:Vector2i
 var object:Dictionary = {
-	0: {
-		"seasons" = {
-			"spring" = {
-				"default" = load("res://assets/resources/buildings/sign/spring/object_0.png"),
-				"hovered" = load("res://assets/resources/buildings/sign/spring/object_1.png"),
-				"delete" = load("res://assets/resources/buildings/sign/spring/object_2.png")
-			},
-			"summer" = {
-				"default" = load("res://assets/resources/buildings/sign/summer/object_0.png"),
-				"hovered" = load("res://assets/resources/buildings/sign/summer/object_1.png"),
-				"delete" = load("res://assets/resources/buildings/sign/summer/object_2.png")
-			},
-			"autumn" = {
-				"default" = load("res://assets/resources/buildings/sign/autumn/object_0.png"),
-				"hovered" = load("res://assets/resources/buildings/sign/autumn/object_1.png"),
-				"delete" = load("res://assets/resources/buildings/sign/autumn/object_2.png")
-			},
-			"winter" = {
-				"default" = load("res://assets/resources/buildings/sign/winter/object_0.png"),
-				"hovered" = load("res://assets/resources/buildings/sign/winter/object_1.png"),
-				"delete" = load("res://assets/resources/buildings/sign/winter/object_2.png")
-			},
-		}
+	"seasons" = {
+		"spring" = {
+			"default" = load("res://assets/resources/buildings/sign/spring/object_0.png"),
+			"hovered" = load("res://assets/resources/buildings/sign/spring/object_1.png"),
+			"delete" = load("res://assets/resources/buildings/sign/spring/object_2.png")
+		},
+		"summer" = {
+			"default" = load("res://assets/resources/buildings/sign/summer/object_0.png"),
+			"hovered" = load("res://assets/resources/buildings/sign/summer/object_1.png"),
+			"delete" = load("res://assets/resources/buildings/sign/summer/object_2.png")
+		},
+		"autumn" = {
+			"default" = load("res://assets/resources/buildings/sign/autumn/object_0.png"),
+			"hovered" = load("res://assets/resources/buildings/sign/autumn/object_1.png"),
+			"delete" = load("res://assets/resources/buildings/sign/autumn/object_2.png")
+		},
+		"winter" = {
+			"default" = load("res://assets/resources/buildings/sign/winter/object_0.png"),
+			"hovered" = load("res://assets/resources/buildings/sign/winter/object_1.png"),
+			"delete" = load("res://assets/resources/buildings/sign/winter/object_2.png")
+		},
 	}
 }
 
@@ -59,12 +56,12 @@ func _ready():
 
 func update() -> void:
 	if clock:
-		if object[level].has("seasons"):
+		if object.has("seasons"):
 			var season = clock.get_season()
-			if object[level]["seasons"].has(season):
-				if object[level]["seasons"][season].has("default"):
-					if object[level]["seasons"][season]["default"] is CompressedTexture2D:
-						sprite.texture = object[level]["seasons"][season]["default"]
+			if object["seasons"].has(season):
+				if object["seasons"][season].has("default"):
+					if object["seasons"][season]["default"] is CompressedTexture2D:
+						sprite.texture = object["seasons"][season]["default"]
 
 func set_sign_sprite(item_id):
 	sprite_id = item_id
@@ -72,10 +69,6 @@ func set_sign_sprite(item_id):
 		if items.content.has(int(item_id)):
 			if items.content[int(item_id)].has("icon"):
 				icon.texture = items.content[int(item_id)]["icon"]
-			else:
-				data.debug("The object does not have the 'description' key", "error")
-		else:
-			data.debug("Invalid index: " + str(item_id), "error")
 
 func _input(event):
 	if event is InputEventMouseButton\
@@ -98,21 +91,21 @@ func _change_sprite(type:bool) -> void:
 		var distance = round(global_position.distance_to(player.global_position))
 		if grid.mode == grid.modes.NOTHING and distance < max_distance:
 			if clock:
-				if object[level].has("seasons"):
+				if object.has("seasons"):
 					var season = clock.get_season()
-					if object[level]["seasons"].has(season):
-						if object[level]["seasons"][season].has("hovered"):
-							if object[level]["seasons"][season]["hovered"] is CompressedTexture2D:
-								sprite.texture = object[level]["seasons"][season]["hovered"]
+					if object["seasons"].has(season):
+						if object["seasons"][season].has("hovered"):
+							if object["seasons"][season]["hovered"] is CompressedTexture2D:
+								sprite.texture = object["seasons"][season]["hovered"]
 			open_menu = true
 	else:
 		if clock:
-			if object[level].has("seasons"):
+			if object.has("seasons"):
 				var season = clock.get_season()
-				if object[level]["seasons"].has(season):
-					if object[level]["seasons"][season].has("default"):
-						if object[level]["seasons"][season]["default"] is CompressedTexture2D:
-							sprite.texture = object[level]["seasons"][season]["default"]
+				if object["seasons"].has(season):
+					if object["seasons"][season].has("default"):
+						if object["seasons"][season]["default"] is CompressedTexture2D:
+							sprite.texture = object["seasons"][season]["default"]
 		tip.tooltip("")
 		open_menu = false
 
@@ -141,13 +134,12 @@ func _on_area_2d_mouse_entered() -> void:
 	&& grid.mode == grid.modes.NOTHING\
 	&& buttonDestroy.destroyMode:
 		destroyMode = true
-		if object.has(level):
-			if object[level].has("seasons"):
-				var season = clock.get_season()
-				if object[level]["seasons"].has(season):
-					if object[level]["seasons"][season].has("delete"):
-						if object[level]["seasons"][season]["delete"] is CompressedTexture2D:
-							sprite.texture = object[level]["seasons"][season]["delete"]
+		if object.has("seasons"):
+			var season = clock.get_season()
+			if object["seasons"].has(season):
+				if object["seasons"][season].has("delete"):
+					if object["seasons"][season]["delete"] is CompressedTexture2D:
+						sprite.texture = object["seasons"][season]["delete"]
 
 func _on_area_2d_mouse_exited() -> void:
 	_change_sprite(false)
