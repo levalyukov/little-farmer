b000563130ff22963fe239d709d7a2428e2ce4c0
levalyukov <lev.alyukov@gmail.com>
2025-05-25 11:18:40 +0500

    Update nature.gd

--- assets/scripts/world/nature/nature.gd
+++ assets/scripts/world/nature/nature.gd
@@ -81,32 +81,26 @@ func create_start_nature():
 	while weed_func_called < weed_func_calls:
 		weed_func_called+=1
 		create_natural_obj(weed_node, "weed", weeds, weeds_shadow)
-		print('weed_func_called < weed_func_calls')
 
 	check_aviabled_vectors()
 	while stone_func_called < stone_func_calls:
 		stone_func_called+=1
 		create_natural_obj(stone_node, "stone", stones, stones_shadow)
-		print('stone_func_called < stone_func_calls')
 
 	check_aviabled_vectors()
 	while tree_func_called < tree_func_calls:
 		tree_func_called+=1
 		create_natural_obj(tree_node, "tree", trees, trees_shadow)
-		print('tree_func_called < tree_func_calls')
 
 func create_natural_obj(node:PackedScene, node_name:String, sprites_array:Array[CompressedTexture2D], shadows_array:Array[CompressedTexture2D]) -> void:
 	var target_node = node.instantiate()
 	var target_position = set_random_position()
 	
 	if node == null:
-		data.debug("Node instantiation failed.", "error")
 		return
 	if target_position == null:
-		data.debug("Target position is invalid.", "error")
 		return
 	if tilemap == null:
-		data.debug("Tilemap is null.", "error")
 		return
 	
 	if !collision.check_cell(target_position, collision.road_layer)\
