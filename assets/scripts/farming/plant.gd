99aef1bb025c553a443ad6561059a29ce7f03e4f
levalyukov <lev.alyukov@gmail.com>
2025-05-27 22:47:28 +0500

    Update plant.gd

--- assets/scripts/farming/plant.gd
+++ assets/scripts/farming/plant.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
 @onready var main:String = str(get_tree().root.get_child(2).name)
-@onready var farming:Node2D = get_node("/root/"+main+"/FarmingManager")
-@onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var tip:Control = get_node("/root/"+main+"/UI/Feedback/Tooltip")
-@onready var grid:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid")
-@onready var collision:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid/GridParent")
 # Дети
 @onready var sprite:Sprite2D = $Sprite2D
 @onready var indicator:Sprite2D = $Indicator
@@ -24,27 +20,31 @@ enum PHASES {
 }
 
 # Crops config
-var _caption:String = ''		# Названия культуры, передается только ключ: 'crops.carrot' -> 'Морковь'
-var _growth_rate:float = 0.0	# Время роста
-var _growth_max:int = 0			# Максимальный уровень роста
-var _mortality:int = 0			# Смертность
-var _seasons:Array = []			# Сезоны, в которые культура может жить
-var _rect_y:int = 0				# Координата Y прямоугольника спрайта
-var _fertilize:float = 0.0		# Процент удобрения
-var _position:Vector2i = Vector2i(0,0)
+var _caption:String = ''				# Названия культуры, передается только ключ: 'crops.carrot' -> 'Морковь'
+var _growth_rate:float = 0.0			# Время роста
+var _growth_max:int = 0					# Максимальный уровень роста
+var _mortality:int = 0					# Смертность
+var _seasons:Array = []					# Сезоны, в которые культура может жить
+var _rect_y:int = 0						# Координата Y прямоугольника спрайта
+var _fertilize:float = 0.0				# Процент удобрения
+var _position:Vector2i = Vector2i(0,0)	# Позиция объекта на тайловой карте
+
+var _growth_value:int = 0				# Значение, с которым 'farming.gd' манипулирует для роста растений
 
 func plant(
-		plant_id				:int		,
-		plant_caption			:String		,
-		plant_growth_rate		:float		,
-		plant_growth_level_max	:int		,
-		plant_mortality_value	:int		,
-		plant_seasons_array		:Array		, 
-		plant_rect_y			:int		,
-		plant_condition			:int		,
-		plant_level				:int		,
-		plant_degree			:int		,
-		plant_position			:Vector2i	,
+		plant_id:int,
+		plant_caption:String,
+		plant_growth_rate:float,
+		plant_growth_level_max:int,
+		plant_mortality_value:int,
+		plant_seasons_array:Array, 
+		plant_rect_y:int,
+		plant_condition:int,
+		plant_level:int,
+		plant_degree:int,
+		plant_position:Vector2i,
+		plant_fertilize_percent:int,
+		plant_growth_value:int = 0
 	) -> void:
 	_plant_id = plant_id
 	_caption = plant_caption
@@ -53,6 +53,7 @@ func plant(
 	_mortality = plant_mortality_value
 	_seasons = plant_seasons_array
 	_rect_y = plant_rect_y
+	_fertilize = plant_fertilize_percent
 
 	_condition = plant_condition
 	_level = plant_level
@@ -60,24 +61,18 @@ func plant(
 	_position = plant_position
 	self.set_position(_position)
 
+	if plant_growth_value == 0:
+		_growth_value = round(plant_growth_rate - (plant_fertilize_percent / 100.0) * plant_growth_rate)
+	else:
+		_growth_value = plant_growth_value
+
 	sprite.region_rect.position.x = 0
 	sprite.region_rect.position.y = _rect_y
-
-	if !_condition == PHASES.GROWED\
-	&& _condition == PHASES.DEAD:
-		if !collision.check_cell(
-			tilemap.local_to_map(_position), 
-			collision.watering_layer
-		):
-			_condition = PHASES.PLANTED
-		else:
-			_condition = PHASES.GROWING
 
 func growth() -> void:
 	if _level < _growth_max:
 		_level = min(_level + 1, _growth_max)
 		sprite.region_rect.position.x = _level * 16
-		tilemap.set_cells_terrain_connect(collision.watering_layer,[tilemap.local_to_map(_position)],0,-1)
 	else:
 		_condition = PHASES.GROWED
 
@@ -98,36 +93,42 @@ func _get_condition_local(condition_type:int) -> String:
 
 func get_data() -> Dictionary:
 	return {
-		#	"position": tilemap.local_to_map(global_position),
-		#	"time_left": timer.get_time_left(),
-		#	"plant_id": _plant_id,
-		#	"caption": _caption,
-		#	"condition_value": _condition,
-		#	"fertilizer": _fertilize,
-		#	"growth_level": _level,
-		#	"growth_rate": _growth_rate,
-		#	"growth_level_max": _growth_max,
-		#	"degree_value": _degree,
-		#	"mortality": _mortality,
-		#	"seasons": _seasons,
-		#	"rect_Y": _rect_y,
+		"position": _position,
+		"plant_id": _plant_id,
+		"caption": _caption,
+		"condition": _condition,
+		"fertilizer_percent": _fertilize,
+		"level": _level,
+		"growth_rate": _growth_rate,
+		"growth_value": _growth_value,
+		"level_max": _growth_max,
+		"degree": _degree,
+		"mortality": _mortality,
+		"seasons": _seasons,
+		"rect_y": _rect_y,
 	}
 
 func _on_collision_mouse_entered():
-	if grid.mode == grid.modes.NOTHING:
-		if !tip.visible:
+	if !tip.visible:
+		if abs(_fertilize) == 0.0:
 			tip.tooltip(
 				tr(_caption) + '\n'
 				+ tr('tooltip.plant_condition') + ": " + str(_get_condition_local(_condition))
+			)
+		else:
+			tip.tooltip(
+				tr(_caption) + '\n'
+				+ tr('tooltip.plant_condition') + ": " + str(_get_condition_local(_condition)) + '\n'
+				+ tr('tooltip.plant_fertilized')
 			)
 
 func _on_collision_mouse_exited():
 	tip.tooltip()
 
 func _exit_tree() -> void:
-	_plant_id = -1
+	_plant_id = 0
 	_level = 0
-	_condition = -1
+	_condition = 0
 	_degree = 0
 	_caption = ''
 	_growth_rate = 0.0
@@ -136,3 +137,4 @@ func _exit_tree() -> void:
 	_seasons = []
 	_rect_y = 0
 	_fertilize = 0.0
+	_growth_value = 0
