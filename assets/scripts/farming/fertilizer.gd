7f8c9256fd1f1937a04795f2a4e1157c9b0f5d28
levalyukov <lev.alyukov@gmail.com>
2025-05-27 22:47:28 +0500

    Update fertilizer.gd

--- assets/scripts/farming/fertilizer.gd
+++ assets/scripts/farming/fertilizer.gd
@@ -4,12 +4,13 @@ extends Node2D
 @onready var tilemap:TileMap = get_node("/root/"+main+"/Tilemap")
 @onready var sprite:Sprite2D = $Sprite2D
 
-var items:Object = Items.new()
-var id:int
+var _id:int = 0
+var _percent:int = 0
 
-func set_fertilizer(fertilizerId:int) -> void:
-	id = fertilizerId
-	match fertilizerId:
+func set_fertilizer(_fertilizer_id:int, _fertilizer_percent:int) -> void:
+	_id = _fertilizer_id
+	_percent = _fertilizer_percent
+	match _id:
 		61:
 			sprite.region_rect.position.x = 0
 			sprite.region_rect.position.y = 0
@@ -21,21 +22,23 @@ func set_fertilizer(fertilizerId:int) -> void:
 	
 func get_data() -> Dictionary:
 	return {
-		"fertilizerID": id,
+		"id": _id,
+		"percent": _percent,
 		"region_rect.x": sprite.region_rect.position.x,
 		"region_rect.y": sprite.region_rect.position.y,
 		"position": tilemap.local_to_map(global_position),
 	}
 
 func set_data(
-		fertilizerID:int, 
-		region_rect_x:int, 
-		region_rect_y:int, 
-		vector:Vector2i,
-		caption:String,
+		_fertilizer_id:int,
+		_fertilizer_percent:int, 
+		_region_rect_x:int, 
+		_region_rect_y:int, 
+		_position:Vector2i,
+		_caption:String,
 	) -> void:
-	id = fertilizerID
-	sprite.region_rect.position.x = region_rect_x
-	sprite.region_rect.position.y = region_rect_y
-	self.name = caption
-	set_position(tilemap.map_to_local(vector))
+	_id = _fertilizer_id
+	sprite.region_rect.position.x = _region_rect_x
+	sprite.region_rect.position.y = _region_rect_y
+	self.name = _caption
+	set_position(tilemap.map_to_local(_position))
