b443f7bab649af00ef6a281f14d0833ff8cda0aa
levalyukov <lev.alyukov@gmail.com>
2025-07-03 13:41:57 +0500

    update blackout: add new animation

--- assets/scripts/ui/decorative/blackout.gd
+++ assets/scripts/ui/decorative/blackout.gd
@@ -1,60 +1,40 @@
 extends Control
 
-@onready var main = str(get_tree().root.get_child(2).name)
-@onready var data = get_node("/root/"+main)
 @onready var background:ColorRect = $ColorRect
-@onready var anim:AnimationPlayer = $Animation
 
-var blackouted:bool = false
+const MAX:float = 1.0
+const MIN:float = 0.0
+const SPEED:float = 0.001
+const EPSILON:float = 0.0001
+
+var value:float = 0.0
+var state:bool = false
+var reverse:bool = false
 
 func _ready():
 	z_index = 999
-	background.modulate = "ffffff"
 
-func blackout(state:bool, speed:int = 4) -> void:
-	match state:
-		true:
-			anim.play("blackout")
-			anim.speed_scale = speed
-			blackouted = true
-		false:
-			anim.play("blackout_reset")
-			anim.speed_scale = speed
-			blackouted = false
+func _process(_delta):
+	if state: 
+		value = min(value + SPEED, MAX)
+	else: 
+		value = max(value - SPEED, MIN)
 
-func change_color(colouring:Color, default_clear_color:bool = false):
-	if typeof(colouring) == TYPE_COLOR:
-		background.color = colouring
-		if default_clear_color:
-			RenderingServer.set_default_clear_color(colouring)
-	else:
-		if data.has_method("debug"):
-			data.debug("The variable 'coloring' is not a type of 'Color'.", "error")
+	if state && abs(value - MAX) < EPSILON:
+		set_process(false)
+	elif !state && abs(value - MIN) < EPSILON:
+		set_process(false)
+	background.material.set_shader_parameter("progress", value)
 
-func change_scene(path:String) -> bool:
-	if path != "":
-		await get_tree().create_timer(1.25).timeout
-		var result = get_tree().change_scene_to_file(path)
-		
-		if result == OK:
-			return true
-		else:
-			match result:
-				ERR_CANT_OPEN:
-					if data.has_method("debug"):
-						data.debug("Can't open the scene file.", "error")
-				ERR_FILE_NOT_FOUND:
-						data.debug("Scene file not found.", "error")
-				ERR_INVALID_DATA:
-					if data.has_method("debug"):
-						data.debug("Invalid scene file format.", "error")
-				ERR_FILE_CORRUPT:
-					if data.has_method("debug"):
-						data.debug("Scene file is corrupt.", "error")
-				ERR_UNAVAILABLE:
-					if data.has_method("debug"):
-						data.debug("The scene is unavailable.", "error")
-				_:
-					if data.has_method("debug"):
-						data.debug("An unknown error occurred: " + str(result), "error")
-	return false
+func blackout(_state:bool, _start_value:float = 0.0, _reversed:bool = false) -> void:
+	value = _start_value
+	state = _state
+	reverse = _reversed
+	background.material.set_shader_parameter("progress", value)
+	background.material.set_shader_parameter("fill", _reversed)
+	set_process(true)
+
+func change_scene(_path:String, _time_to_change:float = 1.25) -> void:
+	if _path != "":
+		await get_tree().create_timer(_time_to_change).timeout
+		get_tree().change_scene_to_file(_path)
