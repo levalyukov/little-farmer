cbb010594d69ce96d3449ba8a416b9276cebcee7
levalyukov <lev.alyukov@gmail.com>
2025-07-07 12:05:27 +0500

    Update zoom.gd

--- assets/scripts/player/zoom.gd
+++ assets/scripts/player/zoom.gd
@@ -6,13 +6,15 @@ extends Camera2D
 @onready var grid:Node2D = get_node("/root/"+main+"/ConstructionManager/Grid")
 @onready var player:CharacterBody2D = get_node("/root/"+main+"/Player")
 
-const camera_speed:float = 1000.0
-const max_offset_distance:float = 150.0
-const center_deadzone_radius_screen:float = 150.0
+var zoomSound:Resource = preload('res://assets/sounds/ui/zoom.ogg')
+
+#	const camera_speed:float = 1000.0
+#	const max_offset_distance:float = 150.0
+#	const center_deadzone_radius_screen:float = 150.0
 
 const zoom_min:float = 2.0
 const zoom_max:float = 10.0
-const zoom_speed:float = 6.0
+const zoom_speed:float = 7.5
 const zoom_tolerance:float = 0.01
 const zoom_increment:float = 1.0
 
@@ -25,6 +27,7 @@ var audio = AudioStreamPlayer.new()
 
 func _ready():
 	self.add_child(audio)
+	audio.stream = zoomSound
 
 func _process(delta) -> void:
 	if !pause.paused:
@@ -37,16 +40,12 @@ func handle_zoom(delta) -> void:
 			if current_zoom < zoom_max:
 				target_zoom = min(current_zoom + zoom_increment, zoom_max)
 				is_changing_zoom = true
-				if audio:
-					audio.stream = load('res://assets/sounds/ui/zoom.ogg')
-					audio.play()
+				if audio: audio.play()
 		if Input.is_action_just_released("mouse wheel down"):
 			if current_zoom > zoom_min:
 				target_zoom = max(current_zoom - zoom_increment, zoom_min)
 				is_changing_zoom = true
-				if audio:
-					audio.stream = load('res://assets/sounds/ui/zoom.ogg')
-					audio.play()
+				if audio: audio.play()
 
 	if is_changing_zoom:
 		current_zoom = lerp(current_zoom, target_zoom, zoom_speed * delta)
@@ -55,33 +54,33 @@ func handle_zoom(delta) -> void:
 			is_changing_zoom = false
 		set_zoom(Vector2(current_zoom, current_zoom))
 
-func handle_mouse_look(delta) -> void:
-	var mouse_position_screen:Vector2 = get_viewport().get_mouse_position()
-	var viewport_size:Vector2 = get_viewport().get_visible_rect().size
-	var mouse_position:Vector2 = get_global_mouse_position()
-	var player_position:Vector2 = player.global_position
-	var direction:Vector2 = mouse_position - player_position
-	var center_deadzone_radius_world:float = center_deadzone_radius_screen / current_zoom
-	if is_mouse_inside_viewport(mouse_position_screen, viewport_size):
-		if !pause.other_menu && grid.mode == grid.modes.NOTHING:
-			if direction.length() > center_deadzone_radius_world:
-				var movement:Vector2 = direction.normalized() * camera_speed * delta / current_zoom
-				global_position += movement
-				var offset_from_player:Vector2 = global_position - player_position
-				if offset_from_player.length() > max_offset_distance / current_zoom:
-					offset_from_player = offset_from_player.normalized() * max_offset_distance / current_zoom
-					global_position = player_position + offset_from_player
-			else:
-				global_position = player_position
-		else:
-			global_position = player_position
-	else:
-		global_position = player_position
+#	func handle_mouse_look(delta) -> void:
+#		var mouse_position_screen:Vector2 = get_viewport().get_mouse_position()
+#		var viewport_size:Vector2 = get_viewport().get_visible_rect().size
+#		var mouse_position:Vector2 = get_global_mouse_position()
+#		var player_position:Vector2 = player.global_position
+#		var direction:Vector2 = mouse_position - player_position
+#		var center_deadzone_radius_world:float = center_deadzone_radius_screen / current_zoom
+#		if is_mouse_inside_viewport(mouse_position_screen, viewport_size):
+#			if !pause.other_menu && grid.mode == grid.modes.NOTHING:
+#				if direction.length() > center_deadzone_radius_world:
+#					var movement:Vector2 = direction.normalized() * camera_speed * delta / current_zoom
+#					global_position += movement
+#					var offset_from_player:Vector2 = global_position - player_position
+#					if offset_from_player.length() > max_offset_distance / current_zoom:
+#						offset_from_player = offset_from_player.normalized() * max_offset_distance / current_zoom
+#						global_position = player_position + offset_from_player
+#				else:
+#					global_position = player_position
+#			else:
+#				global_position = player_position
+#		else:
+#			global_position = player_position
 
-func is_mouse_inside_viewport(mouse_position:Vector2, viewport_size:Vector2) -> bool:
-	return (
-		mouse_position.x >= 0 &&
-		mouse_position.y >= 0 &&
-		mouse_position.x <= viewport_size.x &&
-		mouse_position.y <= viewport_size.y
-	)
+# func is_mouse_inside_viewport(mouse_position:Vector2, viewport_size:Vector2) -> bool:
+# 	return (
+# 		mouse_position.x >= 0 &&
+# 		mouse_position.y >= 0 &&
+# 		mouse_position.x <= viewport_size.x &&
+# 		mouse_position.y <= viewport_size.y
+# 	)
