164a126d4d3ec167e10e873ab11d0c900d2def30
levalyukov <lev.alyukov@gmail.com>
2025-07-11 00:01:10 +0500

    Update player_movement.gd

--- assets/scripts/player/player_movement.gd
+++ assets/scripts/player/player_movement.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SMOOTHING_SPEED:int = 5
 const SPEED:int = 150
-const THRESHOLD:int = 100
+const THRESHOLD:int = 50
 
 var direction:Vector2 = Vector2.ZERO
 var switch:bool = true
@@ -23,26 +23,28 @@ func check_switch() -> void:
 		$Camera2D.position_smoothing_speed = SMOOTHING_SPEED
 		
 func _physics_process(_delta):
-	if !switch && !mouseMovement:
-		velocity = direction * SPEED
+	if !switch:
+		if !mouseMovement:
+			velocity = direction * SPEED
+		
+		if mouseMovement:
+			var _viewport_size = get_viewport_rect().size
+			var _mouse_position = get_viewport().get_mouse_position()
 
-	if mouseMovement:
-		var _viewport_size = get_viewport_rect().size
-		var _mouse_position = get_viewport().get_mouse_position()
+			direction = Vector2i.ZERO
 
-		direction = Vector2i.ZERO
+			var center_threshold = THRESHOLD * 2.5
+			var center_x = _viewport_size.x / 2
+			var center_y = _viewport_size.y / 2
 
-		var center_threshold = THRESHOLD * 2.5
-		var center_x = _viewport_size.x / 2
-		var center_y = _viewport_size.y / 2
+			if !(_mouse_position.x > center_x - center_threshold\
+			&& _mouse_position.x < center_x + center_threshold\
+			&& _mouse_position.y > center_y - center_threshold\
+			&& _mouse_position.y < center_y + center_threshold):
+				if _mouse_position.x > _viewport_size.x - THRESHOLD: direction.x = 1
+				if _mouse_position.x < THRESHOLD: direction.x = -1
+				if _mouse_position.y < THRESHOLD: direction.y = -1
+				if _mouse_position.y > _viewport_size.y - THRESHOLD: direction.y = 1
 
-		if !(_mouse_position.x > center_x - center_threshold\
-		&& _mouse_position.x < center_x + center_threshold\
-		&& _mouse_position.y > center_y - center_threshold\
-		&& _mouse_position.y < center_y + center_threshold):
-			if _mouse_position.x > _viewport_size.x - THRESHOLD: direction.x = 1
-			if _mouse_position.x < THRESHOLD: direction.x = -1
-			if _mouse_position.y < THRESHOLD: direction.y = -1
-			if _mouse_position.y > _viewport_size.y - THRESHOLD: direction.y = 1
-		velocity = direction * SPEED
-	move_and_slide()
+			velocity = direction * SPEED
+		move_and_slide()
