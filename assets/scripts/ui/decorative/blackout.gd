a860f1b1174868bee76756465983a76243dd64ed
levalyukov <lev.alyukov@gmail.com>
2025-07-11 00:01:17 +0500

    Update blackout.gd

--- assets/scripts/ui/decorative/blackout.gd
+++ assets/scripts/ui/decorative/blackout.gd
@@ -1,40 +1,29 @@
 extends Control
 
 @onready var background:ColorRect = $ColorRect
-
-const MAX:float = 1.0
-const MIN:float = 0.0
-const SPEED:float = 0.001
-const EPSILON:float = 0.0001
-
-var value:float = 0.0
-var state:bool = false
-var reverse:bool = false
+@onready var anim:AnimationPlayer = $Animation
 
 func _ready():
 	z_index = 999
+	background.modulate = "ffffff"
 
-func _process(_delta):
-	if state: 
-		value = min(value + SPEED, MAX)
-	else: 
-		value = max(value - SPEED, MIN)
+func blackout(state:bool, speed:float = 0.5) -> void:
+	match state:
+		true:
+			anim.play("blackout")
+			anim.speed_scale = speed
+			
+		false:
+			anim.play("blackout_reset")
+			anim.speed_scale = speed
 
-	if state && abs(value - MAX) < EPSILON:
-		set_process(false)
-	elif !state && abs(value - MIN) < EPSILON:
-		set_process(false)
-	background.material.set_shader_parameter("progress", value)
+func change_color(colouring:Color, default_clear_color:bool = false):
+	if typeof(colouring) == TYPE_COLOR:
+		background.color = colouring
+		if default_clear_color:
+			RenderingServer.set_default_clear_color(colouring)
 
-func blackout(_state:bool, _start_value:float = 0.0, _reversed:bool = false) -> void:
-	value = _start_value
-	state = _state
-	reverse = _reversed
-	background.material.set_shader_parameter("progress", value)
-	background.material.set_shader_parameter("fill", _reversed)
-	set_process(true)
-
-func change_scene(_path:String, _time_to_change:float = 1.25) -> void:
+func change_scene(_path:String, _value:float = 1.25) -> void:
 	if _path != "":
-		await get_tree().create_timer(_time_to_change).timeout
+		await get_tree().create_timer(_value).timeout
 		get_tree().change_scene_to_file(_path)
