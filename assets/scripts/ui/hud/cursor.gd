b830fcd36faa2fe09f5f7ae43362e7e179f7d6c7
levalyukov <lev.alyukov@gmail.com>
2026-07-14 19:25:58 +0500

    Update cursor.gd

--- assets/scripts/ui/hud/cursor.gd
+++ assets/scripts/ui/hud/cursor.gd
@@ -1,104 +1,94 @@
 extends Node2D
 
-@onready var main:String = str(get_tree().root.get_child(2).name)
-@onready var tooltip:Control = get_node("/root/"+main+"/UI/Feedback/Tooltip")
+# =============================================================================================
+#  (cursor.gd)
+# =============================================================================================
+# Меняет системную мышь на игровую текстуру
+#
+# ЗОНА ОТВЕТСТВЕННОСТИ:
+# - Смена состояния игрового курсора
+#
+# ОСНОВНОЙ ФУНКЦИОНАЛ:
+# - set_cursor(new_state:STATES) - смена состояния курсора
+#
+# =============================================================================================
 
-enum states {DEFAULT, EXPLORE, ACTIVE}
-var state:int = states.DEFAULT
-var cursor:Dictionary = {
-	"default": {
-		"static": load("res://assets/resources/ui/interactive/hud/cursor/cursor_static.png"),
-		"active": load("res://assets/resources/ui/interactive/hud/cursor/cursor_active.png"),
+enum STATES {DEFAULT, ACTIVE}
+const CURSOR:Dictionary = \
+{
+	STATES.DEFAULT: \
+	{
+		"static": preload("res://assets/resources/ui/interactive/hud/cursor/cursor_static.png"),
+		"active": preload("res://assets/resources/ui/interactive/hud/cursor/cursor_active.png"),
 	},
-	"explore": {
-		"static": load("res://assets/resources/ui/interactive/hud/cursor/explore.png"),
-	},
-	'active': {
-		"static": load("res://assets/resources/ui/interactive/hud/cursor/clicked_default.png"),
-		"active": load("res://assets/resources/ui/interactive/hud/cursor/clicked_active.png"),
+
+	STATES.ACTIVE: \
+	{
+		"static": preload("res://assets/resources/ui/interactive/hud/cursor/clicked_default.png"),
+		"active": preload("res://assets/resources/ui/interactive/hud/cursor/clicked_active.png"),
 	}
 }
 
-func _ready():
-	set_cursor(state)
+var state:STATES = STATES.DEFAULT
 
-func set_cursor(cursorState:int) -> void:
-	state = cursorState
-	match cursorState:
-		states.DEFAULT:
-			if cursor.has("default"):
-				if cursor["default"].has("static"):
-					Input.set_custom_mouse_cursor(
-						cursor["default"]["static"], 
-						Input.CURSOR_ARROW, 
-						Vector2(0,0)
-						)
-		states.EXPLORE:
-			if cursor.has("explore"):
-				if cursor["explore"].has("static"):
-					Input.set_custom_mouse_cursor(
-						cursor["explore"]["static"],
-						Input.CURSOR_ARROW, 
-						Vector2(0,0)
-						)
-		states.ACTIVE:
-			if cursor.has("active"):
-				if cursor["active"].has("static"):
-					Input.set_custom_mouse_cursor(
-						cursor["active"]["static"],
-						Input.CURSOR_ARROW, 
-						Vector2(0,0)
-						)
+func _ready() -> void:
+	set_cursor(STATES.DEFAULT)
 
-func _input(event) -> void:
+func set_cursor(new_state:STATES) -> void:
+	state = new_state
+	if CURSOR.has(state)\
+	&& CURSOR[state].has("static"):
+		Input.set_custom_mouse_cursor(
+			CURSOR[state]["static"], 
+			Input.CURSOR_ARROW, 
+			Vector2(0,0)
+		)
+
+func _input(event:InputEvent) -> void:
+	if !CURSOR.has(state):
+		return
+
 	match state:
-		states.DEFAULT:
-			if cursor.has("default"):
-				if cursor["default"].has("active"):
-					if event is InputEventMouseButton\
-					&& event.button_index == MOUSE_BUTTON_LEFT\
-					&& event.is_pressed():
-						Input.set_custom_mouse_cursor(
-							cursor["default"]["active"],
-							Input.CURSOR_ARROW, 
-							Vector2(0,0)
-							)
-				if cursor["default"].has("static"):
-					if event is InputEventMouseButton\
-					&& event.button_index == MOUSE_BUTTON_LEFT\
-					&& event.is_released():
-						Input.set_custom_mouse_cursor(
-							cursor["default"]["static"],
-							Input.CURSOR_ARROW, 
-							Vector2(0,0)
-							)
-		states.EXPLORE:
-			if cursor.has("explore"):
-				if cursor["explore"].has("static"):
-					Input.set_custom_mouse_cursor(
-						cursor["explore"]["static"],
-						Input.CURSOR_ARROW, 
-						Vector2(0,0)
-						)
-		states.ACTIVE:
-			if cursor.has("active"):
-				if cursor["active"].has("active"):
-					if event is InputEventMouseButton\
-					&& event.button_index == MOUSE_BUTTON_LEFT\
-					&& event.is_pressed():
-						Input.set_custom_mouse_cursor(
-							cursor["active"]["active"],
-							Input.CURSOR_ARROW, 
-							Vector2(0,0)
-							)
-				if cursor["active"].has("static"):
-					if event is InputEventMouseButton\
-					&& event.button_index == MOUSE_BUTTON_LEFT\
-					&& event.is_released():
-						Input.set_custom_mouse_cursor(
-							cursor["active"]["static"],
-							Input.CURSOR_ARROW, 
-							Vector2(0,0)
-							)
-		_:
-			pass
+		STATES.DEFAULT:
+			if CURSOR[state].has("static")\
+			&& event is InputEventMouseButton\
+			&& event.button_index == MOUSE_BUTTON_LEFT\
+			&& event.is_released():
+				Input.set_custom_mouse_cursor(
+					CURSOR[state]["static"],
+					Input.CURSOR_ARROW, 
+					Vector2(0,0)
+				)
+
+			if CURSOR[state].has("active")\
+			&& event is InputEventMouseButton\
+			&& event.button_index == MOUSE_BUTTON_LEFT\
+			&& event.is_pressed():
+				Input.set_custom_mouse_cursor(
+					CURSOR[state]["active"],
+					Input.CURSOR_ARROW, 
+					Vector2(0,0)
+				)
+
+		STATES.ACTIVE:
+			if CURSOR[state].has("active")\
+			&& event is InputEventMouseButton\
+			&& event.button_index == MOUSE_BUTTON_LEFT\
+			&& event.is_pressed():
+				Input.set_custom_mouse_cursor(
+					CURSOR[state]["active"],
+					Input.CURSOR_ARROW, 
+					Vector2(0,0)
+				)
+
+			if CURSOR[state].has("static")\
+			&& event is InputEventMouseButton\
+			&& event.button_index == MOUSE_BUTTON_LEFT\
+			&& event.is_released():
+				Input.set_custom_mouse_cursor(
+					CURSOR[state]["static"],
+					Input.CURSOR_ARROW, 
+					Vector2(0,0)
+				)
+
+		_: pass
