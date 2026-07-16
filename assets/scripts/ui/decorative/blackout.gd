b1aa46635bd5bbcf78c19878299b8fd3b0405fc2
levalyukov <lev.alyukov@gmail.com>
2026-07-17 00:19:19 +0500

    remove unused functions

--- assets/scripts/ui/decorative/blackout.gd
+++ assets/scripts/ui/decorative/blackout.gd
@@ -18,14 +18,3 @@ func blackout(state:bool, speed:float = 0.5) -> void:
 			anim.play("blackout_reset")
 			anim.speed_scale = speed
 			self.mouse_filter = MOUSE_FILTER_IGNORE
-
-func change_color(colouring:Color, default_clear_color:bool = false):
-	if typeof(colouring) == TYPE_COLOR:
-		background.color = colouring
-		if default_clear_color:
-			RenderingServer.set_default_clear_color(colouring)
-
-func change_scene(_path:String, _value:float = 1.25) -> void:
-	if _path != "":
-		await get_tree().create_timer(_value).timeout
-		get_tree().change_scene_to_file(_path)
