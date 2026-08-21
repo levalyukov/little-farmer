34516e349365181bacfefa5cbf278bda73ad8e18
levalyukov <lev.alyukov@gmail.com>
2026-08-21 07:57:46 +0500

    push _anim_is_finished into lambda

--- assets/scripts/ui/menu/credits.gd
+++ assets/scripts/ui/menu/credits.gd
@@ -1,40 +1,23 @@
 extends Control
 
-# =============================================================================================
-# (credits.gd)
-# =============================================================================================
-# Окно титров игры в главном меню
-#
-# ЗОНА ОТВЕТСТВЕННОСТИ:
-# - Открытие окна с авторами и инструментами игры
-#
-# ОСНОВНОЙ ФУНКЦИОНАЛ:
-# - open()
-# - close()
-#
-# ЗАВИСИМОСТИ:
-# - Blur - для размытия фона
-# - Cursor - для изменений состояний
-#
-# =============================================================================================
-
 @onready var anim: AnimationPlayer = $Animation
 @onready var title: Label = $MainContainer/VBox/HeaderContainer/Title
 @onready var general: RichTextLabel = $MainContainer/VBox/ContentContainer/Content
 @onready var button: Button = $MainContainer/VBox/ButtonsContainer/HBox/ConfirmButtonMargin/Confirm
 
-var title_text: String = tr("menu.credits_title")
-var general_text: String = tr("menu.credits_content")
-
 
 func _ready() -> void:
-	title.text = title_text
-	general.text = general_text
+	title.text = tr("menu.credits_title")
+	general.text = tr("menu.credits_content")
 
 	button.pressed.connect(close)
 	button.mouse_entered.connect(_button_hovered)
 	button.mouse_exited.connect(_button_exited)
-	anim.animation_finished.connect(_anim_is_finished)
+	anim.animation_finished.connect(
+		func(anim_name: StringName) -> void:
+			if anim_name != "show":
+				UIManager.remove_ui(self)
+	)
 
 	open()
 
@@ -60,8 +43,3 @@ func _button_hovered() -> void:
 func _button_exited() -> void:
 	SoundManager.play_sound("ui/hover")
 	UIManager.cursor.set_cursor(UIManager.cursor.STATES.DEFAULT)
-
-
-func _anim_is_finished(anim_name: String) -> void:
-	if anim_name != "show":
-		UIManager.ui_remove(self)
