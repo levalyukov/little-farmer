f5a66ad042ced072aa4cc2556fe2042546a6802a
levalyukov <lev.alyukov@gmail.com>
2026-09-09 00:10:12 +0500

    Update player_control.gd

--- assets/scripts/managers/player_control.gd
+++ assets/scripts/managers/player_control.gd
@@ -1,49 +1,28 @@
 extends Node
 
-# ===================================================================
-# PlayerControl (player_control.gd)
-# ===================================================================
-# Синглтон, отвечающий за управление данными игрока: 
-# чертежи, инвентарь, почта, статистика и так далее.
-#
-# ЗОНА ОТВЕТСТВЕННОСТИ:
-# -  
-#
-# ОСНОВНОЙ ФУНКЦИОНАЛ:
-# - 
-#
-# ЗАВИСИМОСТИ:
-# - 
-#
-# ===================================================================
-
-var mailbox:Array[int]
-var inventory:Array[int]
-var blueprints:Dictionary =\
+var balance: int = 0
+var mailbox: Dictionary = {}
+var inventory: Dictionary = {}
+var blueprints: Dictionary = \
 {
-    BlueprintsManager.BlueprintsType.BUILDINGS:
-    [0,1,2,3,4,5,6,7,8,9], 
-
-    BlueprintsManager.BlueprintsType.TERRAINS:
-    [0,1]
+	Blueprints.BlueprintsType.BUILDINGS: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+	Blueprints.BlueprintsType.TERRAINS: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 }
 
-func player_load(content:Dictionary) -> void:
-    mailbox     = content["mailbox"]    if content.has("mailbox")       else []
-    inventory   = content["inventory"]  if content.has("inventory")     else []
-    blueprints  = content["blueprints"] if content.has("blueprints")    else []
+
+func player_load(content: Dictionary) -> void:
+	mailbox = content["mailbox"] if content.has("mailbox") else []
+	inventory = content["inventory"] if content.has("inventory") else []
+	blueprints = content["blueprints"] if content.has("blueprints") else {}
+
 
 func player_get() -> Dictionary:
-    return \
-    {
-        "mailbox":    self.mailbox,
-        "inventory":  self.inventory,
-        "blueprints": 
-            {
-                BlueprintsManager.BlueprintsType.BUILDINGS: 
-                    self.blueprints[BlueprintsManager.BlueprintsType.BUILDINGS],
-                BlueprintsManager.BlueprintsType.TERRAINS: 
-                    self.blueprints[BlueprintsManager.BlueprintsType.TERRAINS],
-            }
-    }
-
+	return {
+		"mailbox": self.mailbox,
+		"inventory": self.inventory,
+		"blueprints":
+		{
+			Blueprints.BlueprintsType.BUILDINGS: self.blueprints[Blueprints.BlueprintsType.BUILDINGS],
+			Blueprints.BlueprintsType.TERRAINS: self.blueprints[Blueprints.BlueprintsType.TERRAINS],
+		}
+	}
