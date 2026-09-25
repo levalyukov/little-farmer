0d64ce054bcc40cd5a9b1e3865accae95478bb8d
levalyukov <lev.alyukov@gmail.com>
2026-09-25 15:17:34 +0500

    Update sounds.gd

--- assets/scripts/managers/sounds.gd
+++ assets/scripts/managers/sounds.gd
@@ -1,35 +1,65 @@
 extends Node
 
-# ===================================================================
-# SoundsManager (sounds.gd)
-# ===================================================================
-# Центральный синглтон отвечающий за воспроизведение игровых звуков
-# и игровой музыки по заданном расписанию.
-#
-# ЗОНА ОТВЕТСТВЕННОСТИ:
-# - Воспроизведение музыки и игровых звуков
-#
-# ОСНОВНОЙ ФУНКЦИОНАЛ:
-# - play_sound(volume:String) - воспроизвести звук из папки "sounds"
-#
-# ЗАВИСИМОСТИ:
-# - AudioStreamPlayer - воспроизведение звуков
-#
-# ===================================================================
+enum MusicType { MENU, GAME }
 
-func play_sound(volume:String) -> void:
-	var path:String
-	var audio:AudioStreamPlayer
+const MENU_FOLDER: String = "res://assets/sounds/music/menu/"
+const RADIO_FOLDER: String = "res://assets/sounds/music/radio/"
+const AMBIENT_FOLDER: String = "res://assets/sounds/music/ambient/"
 
-	path = "res://assets/sounds/"+volume+".ogg" if !volume.ends_with(".ogg")\
-	else "res://assets/sounds/"+volume
+var radio: Array[AudioStreamOggVorbis] = []
 
+
+func _ready() -> void:
+	_init_audio(RADIO_FOLDER)
+
+
+func play_sound(volume: String) -> void:
+	var path: String
+
+	if volume.is_empty():
+		return
+
+	path = "res://assets/sounds/" + volume + ".ogg" if !volume.ends_with(".ogg") else "res://assets/sounds/" + volume
+
+	var audio: AudioStreamPlayer
 	audio = AudioStreamPlayer.new()
-	self.add_child(audio)
 	audio.finished.connect(_audio_is_finished.bind(audio))
 	audio.stream = load(path)
+	self.add_child(audio)
 	audio.play()
 
-func _audio_is_finished(audio:AudioStreamPlayer) -> void:
+
+func play_music(type: MusicType) -> void:
+	match type:
+		MusicType.MENU:
+			pass
+
+		MusicType.GAME:
+			pass
+
+		_:
+			print("asdasdasd")
+
+
+func _init_audio(folder: String) -> void:
+	var dir := DirAccess.open(folder)
+
+	if !dir:
+		printerr("Could not open folder")
+		return
+
+	dir.list_dir_begin()
+	for file: String in dir.get_files():
+		if file.ends_with(".ogg"):
+			var audio := load(dir.get_current_dir() + "/" + file)
+
+			if !audio || !audio is AudioStreamOggVorbis:
+				printerr("Uncorrected type file.")
+				return
+
+			radio.append(audio)
+
+
+func _audio_is_finished(audio: AudioStreamPlayer) -> void:
 	self.remove_child(audio)
 	audio.queue_free()
